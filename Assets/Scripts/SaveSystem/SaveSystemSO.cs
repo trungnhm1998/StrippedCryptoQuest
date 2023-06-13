7322cb87993075c25342a9f741a6ed6ee92a1935
trungnhm1998 <trung.nguyenhoangminh1998@gmail.com>
2023-06-14 00:18:25 +0700

    refactor!: refactor whole project directories with new structure
    - Remove leftover of 2D RPG Kit
    - Remove other leftover
    - Add Input System
    - Add Localize System
    - Add Unity Web Request WWW for WebGL build
    - Add Addressable System

---
+++ Assets/Scripts/SaveSystem/SaveSystemSO.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace CryptoQuest.SaveSystem
+{
+    public class SaveSystemSO : ScriptableObject
+    {
+        public string saveFileName;
+
+        public void SaveData()
+        {
+        }
+
+        public void LoadData()
+        {
+        }
+    }
+}
