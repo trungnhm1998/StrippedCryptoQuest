cd8e86f2c1fb45d3ffe5e6247f8c75c8f6b2ac7c
CA-INDI-0045\minh <minh.nguyen@indigames.net>
2023-06-14 10:38:02 +0700

    feat(package): add Fuzzy finder and SO browser

---
+++ Assets/_Project/Scripts/CharacterSO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSO : ScriptableObject
+{
+ public int id;
+    public string displayName;
+    public bool isMaleGender;
+    public int age;
+    public string birthplace;
+    public string height;
+    public string weight;
+    public string description;
+}
