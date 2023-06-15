78ddf31935d9fdba3832819e44da81a8b84de6db
trungnhm1998 <trung.nguyenhoangminh1998@gmail.com>
2023-06-15 12:57:39 +0700

    test(save-system): implement NullSaveManagerSO
    - add unit test NullSaveManagerSO
    - add unit test SaveSystemSO
    - add unit test SaveData

--- Assets/Scripts/SaveSystem/SaveData.cs
+++ Assets/Scripts/SaveSystem/SaveData.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace CryptoQuest.SaveSystem
 {
     [Serializable]
-    public struct SaveData
+    public class SaveData
     {
-        public string playerName;
+        public string playerName = "";
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public void LoadFromJson(string json)
+        {
+            JsonConvert.PopulateObject(json, this);
+        }
     }
 }
--- Assets/Scripts/SaveSystem/SaveSystemSO.cs
+++ Assets/Scripts/SaveSystem/SaveSystemSO.cs
@@ -5,15 +5,16 @@ namespace CryptoQuest.SaveSystem
     public class SaveSystemSO : ScriptableObject
     {
         [SerializeField] private SaveManagerSO _saveManagerSO;
+        public SaveManagerSO SaveManagerSO => _saveManagerSO;
 
         public SaveData saveData = new SaveData();
 
-        public bool SaveData()
+        public bool SaveGame()
         {
             return _saveManagerSO.Save(saveData);
         }
 
-        public bool LoadData()
+        public bool LoadSaveGame()
         {
             return _saveManagerSO.Load(out saveData);
         }
