37ba59b20043ac69934ae2fd6fc5156c01726f53
Anh Cao <anh.caothilan223@gmail.com>
2023-06-21 16:53:32 +0700

    feat: implemented NPC dialog UI

---
+++ Assets/Scripts/UI/Dialog/SpeechDialog.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Core.Runtime.Events.ScriptableObjects;
+using CryptoQuest.MockData;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CryptoQuest.UI.Dialog
+{
+    public class SpeechDialog : MonoBehaviour
+    {
+        [Header("Events received")]
+        [SerializeField] private VoidEventChannelSO _turnOnDialogEvent;
+        [SerializeField] private VoidEventChannelSO _turnOffDialogEvent;
+
+        [Space]
+        [SerializeField] private NPCSpeechSO _receivedMessage;
+        [SerializeField] private GameObject _innerUI;
+        [SerializeField] private Text _message;
+
+        private void OnEnable()
+        {
+            _turnOnDialogEvent.EventRaised += TurnOnDialog;
+            _turnOffDialogEvent.EventRaised += TurnOffDialog;
+        }
+
+        private void OnDisable()
+        {
+            _turnOnDialogEvent.EventRaised -= TurnOnDialog;
+            _turnOffDialogEvent.EventRaised -= TurnOffDialog;
+        }
+
+        private void TurnOnDialog()
+        {
+            _innerUI.SetActive(true);
+            SetupMessage();
+        }
+
+        private void TurnOffDialog()
+        {
+            _innerUI.SetActive(false);
+        }
+
+        private void SetupMessage()
+        {
+            _message.text = $"{_receivedMessage.Message}";
+        }
+    }
+}
