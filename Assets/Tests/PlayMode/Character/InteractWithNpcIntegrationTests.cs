59af3038468880bda7d49575b6dad8f5bb65063c
Thành Long (WiIIiam) <us.thanhlong18@gmail.com>
2023-06-21 13:34:57 +0700

    feat(interaction): implement integration test

---
+++ Assets/Tests/PlayMode/Character/InteractWithNpcIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Core.Runtime.Character;
+using Core.Runtime.Events.ScriptableObjects.Dialogs;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests.PlayMode.Character
+{
+    public class InteractWithNpcIntegrationTests
+    {
+        [UnityTest]
+        public IEnumerator Interact_WithNpcIntegrationScene_ShouldReturnCorrectDataFromSO()
+        {
+            var npcGameObject = new GameObject("NPC");
+            var npcInstance = npcGameObject.AddComponent<NPC>();
+
+            var dialog = ScriptableObject.CreateInstance<DialogsScriptableObject>();
+
+            dialog.messages = new List<string>() { "Hello World", "Hello World 2" };
+
+            npcInstance.SetDialog(dialog);
+
+            var interactable = npcGameObject.GetComponent<IInteractable>();
+
+            foreach (var message in dialog.messages)
+            {
+                var currentMessage = interactable.Interact();
+
+                Assert.AreEqual(message, currentMessage);
+            }
+
+            yield return null;
+        }
+    }
+}
