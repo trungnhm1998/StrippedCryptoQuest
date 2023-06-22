b69e504df3fad29258e4e033fe7e506e956eda1b
CA-INDI-0045\minh <minh.nguyen@indigames.net>
2023-06-22 13:50:22 +0700

    chore: rm facing logic scripts

--- Assets/Scripts/TeleportArea.cs
+++ Assets/Scripts/TeleportArea.cs
@@ -13,27 +13,18 @@ namespace CryptoQuest
 {
     public class TeleportArea : MonoBehaviour
     {
-        public SceneScriptableObject currentScene;
         public SceneScriptableObject nextScene;
-        public PlayerPositionInfoSO playerPositionInfoSO;
         public LoadSceneEventChannelSO _loadNextSceneEventChannelSO;
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.CompareTag("Player"))
             {
-                PlayerController player = other.gameObject.GetComponent<PlayerController>();
-                UpdatePlayerFacingAxis(player.facingCollider.transform.position - player.transform.position);
                 TriggerTeleport(nextScene);
             }
         }
         private void TriggerTeleport(SceneScriptableObject scene)
         {
             _loadNextSceneEventChannelSO.RequestLoad(scene);
-        }
-
-        private void UpdatePlayerFacingAxis(Vector2 axis)
-        {
-            playerPositionInfoSO.playerFacingAxis = axis;
         }
     }
 }
