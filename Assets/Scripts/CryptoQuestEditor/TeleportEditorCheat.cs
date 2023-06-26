39723b8fdd43e506b18af11b5d89a85580f80097
trungnhm1998 <trung.nguyenhoangminh1998@gmail.com>
2023-06-26 14:17:22 +0700

    refactor: move TeleportEditorCheat into namespace

--- Assets/Scripts/CryptoQuestEditor/TeleportEditorCheat.cs
+++ Assets/Scripts/CryptoQuestEditor/TeleportEditorCheat.cs
@@ -3,48 +3,51 @@ using Core.Runtime.SceneManagementSystem.Events.ScriptableObjects;
 using Core.Runtime.SceneManagementSystem.ScriptableObjects;
 using UnityEngine;
 
-public class TeleportEditorCheat : MonoBehaviour
+namespace CryptoQuestEditor
 {
-#if UNITY_EDITOR || DEVELOPMENT_BUILD
-    public LoadSceneEventChannelSO SceneLoadedEventChannelSO;
-
-    private readonly List<SceneScriptableObject> _sceneScriptableObjects = new List<SceneScriptableObject>();
-    private bool _showSceneList;
-
-    private void OnGUI()
+    public class TeleportEditorCheat : MonoBehaviour
     {
-        _showSceneList = GUILayout.Toggle(_showSceneList, "Show add effects");
-        if (!_showSceneList) return;
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+        public LoadSceneEventChannelSO SceneLoadedEventChannelSO;
 
+        private readonly List<SceneScriptableObject> _sceneScriptableObjects = new List<SceneScriptableObject>();
+        private bool _showSceneList;
 
-        if (_sceneScriptableObjects.Count > 0)
+        private void OnGUI()
         {
-            foreach (var sceneScriptableObject in _sceneScriptableObjects)
+            _showSceneList = GUILayout.Toggle(_showSceneList, "Show add effects");
+            if (!_showSceneList) return;
+
+
+            if (_sceneScriptableObjects.Count > 0)
             {
-                if (GUILayout.Button($"Teleport to {sceneScriptableObject.name}"))
+                foreach (var sceneScriptableObject in _sceneScriptableObjects)
                 {
-                    OnLoadingRequested(sceneScriptableObject);
+                    if (GUILayout.Button($"Teleport to {sceneScriptableObject.name}"))
+                    {
+                        OnLoadingRequested(sceneScriptableObject);
+                    }
                 }
             }
         }
-    }
 
-    private void OnLoadingRequested(SceneScriptableObject sceneScriptableObject)
-    {
-        SceneLoadedEventChannelSO.RequestLoad(sceneScriptableObject);
-    }
-
-    private void Start()
-    {
-        var guids = UnityEditor.AssetDatabase.FindAssets("t:SceneScriptableObject");
-
-        foreach (var guid in guids)
+        private void OnLoadingRequested(SceneScriptableObject sceneScriptableObject)
         {
-            var path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
-            var sceneSO = UnityEditor.AssetDatabase.LoadAssetAtPath<SceneScriptableObject>(path);
-            if (sceneSO.SceneType == SceneScriptableObject.Type.Location)
-                _sceneScriptableObjects.Add(sceneSO);
+            SceneLoadedEventChannelSO.RequestLoad(sceneScriptableObject);
         }
-    }
+
+        private void Start()
+        {
+            var guids = UnityEditor.AssetDatabase.FindAssets("t:SceneScriptableObject");
+
+            foreach (var guid in guids)
+            {
+                var path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+                var sceneSO = UnityEditor.AssetDatabase.LoadAssetAtPath<SceneScriptableObject>(path);
+                if (sceneSO.SceneType == SceneScriptableObject.Type.Location)
+                    _sceneScriptableObjects.Add(sceneSO);
+            }
+        }
 #endif
+    }
 }
