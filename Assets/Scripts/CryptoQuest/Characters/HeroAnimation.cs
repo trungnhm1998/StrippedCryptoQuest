f789b016489af36e9f376811a1c3758748865406
datdiep754129 <dat.diep@indigames.net>
2023-06-27 16:39:29 +0700

    feat: implement animation for character

---
+++ Assets/Scripts/CryptoQuest/Characters/HeroAnimation.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Core.Runtime.EditorTools.Attributes.ReadOnlyAttribute;
+using UnityEngine;
+
+namespace CryptoQuest.Character.MonoBehaviours
+{
+    public class HeroAnimation : MonoBehaviour
+    {
+        [SerializeField] private Animator _heroAnimation;
+        private CharacterBehaviour.EFacingDirection _facingDirection;
+        private Rigidbody2D _rigidbody2D;
+        void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate()
+        {
+            SetAnimation();
+        }
+
+        private void SetAnimation()
+        {
+            if (_rigidbody2D.velocity.x != 0 || _rigidbody2D.velocity.y != 0)
+            {
+                _heroAnimation.SetFloat("moveX", _rigidbody2D.velocity.x);
+                _heroAnimation.SetFloat("moveY", _rigidbody2D.velocity.y);
+            }
+            else
+            {
+                _heroAnimation.SetFloat("moveX", 0);
+                _heroAnimation.SetFloat("moveY", 0);
+            }
+            SetFacingDirection();
+        }
+
+        private void SetFacingDirection()
+        {
+            switch (_rigidbody2D.velocity.x, _rigidbody2D.velocity.y)
+            {
+                case (0, > 0):
+                    _facingDirection = CharacterBehaviour.EFacingDirection.North;
+                    _heroAnimation.SetFloat("lastPosX", 0);
+                    _heroAnimation.SetFloat("lastPosY", .1f);
+                    break;
+                case (0, < 0):
+                    _facingDirection = CharacterBehaviour.EFacingDirection.South;
+                    _heroAnimation.SetFloat("lastPosX", 0);
+                    _heroAnimation.SetFloat("lastPosY", -.1f);
+                    break;
+                case ( < 0, 0):
+                    _facingDirection = CharacterBehaviour.EFacingDirection.West;
+                    _heroAnimation.SetFloat("lastPosX", -.1f);
+                    _heroAnimation.SetFloat("lastPosY", 0);
+                    break;
+                case ( > 0, 0):
+                    _facingDirection = CharacterBehaviour.EFacingDirection.East;
+                    _heroAnimation.SetFloat("lastPosX", .1f);
+                    _heroAnimation.SetFloat("lastPosY", 0);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
