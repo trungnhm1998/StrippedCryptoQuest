c1150004b608cbedacad9199415bbe20041a816e
CA-INDI-0045\minh <minh.nguyen@indigames.net>
2023-06-27 18:00:02 +0700

    feat: set up tron village map exits and entrances

--- Assets/Scripts/CryptoQuest/Map/MapEntrance.cs
+++ Assets/Scripts/CryptoQuest/Map/MapEntrance.cs
@@ -12,18 +12,7 @@ namespace CryptoQuest.Map
         public MapPathSO MapPath => _mapPath;
 
         [SerializeField] private CharacterBehaviour.EFacingDirection _entranceFacingDirection;
-        [SerializeField] private CharacterArgsEventChannelSO _updatePlayerStateEvent;
-        private void Start()
-        {
-            if (_transitionSO.LastTakenPath == null)
-            {
-                return;
-            };
-            if (_mapPath != _transitionSO.LastTakenPath) return;
-            CharacterArgs characterArgs = new CharacterArgs();
-            characterArgs.position = transform.position;
-            characterArgs.facingDirection = _entranceFacingDirection;
-            _updatePlayerStateEvent.RaiseEvent(characterArgs);
-        }
+
+        private void Start() { }
     }
 }
