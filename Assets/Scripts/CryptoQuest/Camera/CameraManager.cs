58de8212d346d191ffbbffe30f0c4e5ae53194fa
Anh Cao <anh.caothilan223@gmail.com>
2023-06-30 13:38:33 +0700

    feat: added moving camera according to the player using Cinemachine

--- Assets/Scripts/CryptoQuest/Camera/CameraManager.cs
+++ Assets/Scripts/CryptoQuest/Camera/CameraManager.cs
@@ -1,8 +1,29 @@
+using Cinemachine;
+using CryptoQuest.Gameplay;
 using UnityEngine;
 
 namespace CryptoQuest.Camera
 {
-    public class CameraManager : MonoBehaviour {
-        
+    public class CameraManager : MonoBehaviour
+    {
+        [SerializeField] GameplayBus _gameplayBus;
+        [SerializeField] CinemachineVirtualCamera _virtualCamera;
+        [SerializeField] private bool _isStaticCamera;
+
+        private void OnEnable() {
+            _gameplayBus.HeroSpawned += HeroSpawned;
+        }
+
+        private void OnDisable() {
+            _gameplayBus.HeroSpawned -= HeroSpawned;
+            
+        }
+
+        private void HeroSpawned()
+        {
+            if (_isStaticCamera) return;
+            var hero = _gameplayBus.Hero;
+            _virtualCamera.Follow = hero.transform;
+        }
     }
 }
