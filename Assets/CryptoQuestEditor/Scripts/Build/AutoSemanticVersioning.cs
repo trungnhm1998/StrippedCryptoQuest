65396187a9d0b82b6b332d0e713d7248b9f81789
trungnhm1998 <trung.nguyenhoangminh1998@gmail.com>
2023-07-06 18:14:13 +0700

    feat: auto increase build while using editor

---
+++ Assets/CryptoQuestEditor/Scripts/Build/AutoSemanticVersioning.cs
@@ -0,0 +1,16 @@
+﻿using IndiGamesEditor.UnityBuilderAction.Versioning;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+namespace CryptoQuestEditor.Build
+{
+    public class AutoSemanticVersioning : IPreprocessBuildWithReport
+    {
+        public int callbackOrder => 0;
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            VersionApplicator.SetVersion(VersionGenerator.Generate());
+        }
+    }
+}
