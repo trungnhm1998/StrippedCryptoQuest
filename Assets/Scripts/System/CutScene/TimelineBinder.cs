a59de15f8f7f82e0cd2309e6c3a0b9af0068b8ba
Thành Long (WiIIiam) <us.thanhlong18@gmail.com>
2023-07-14 12:11:25 +0700

    feat(cutscene): implement birthday cutscene

--- Assets/Scripts/System/CutScene/TimelineBinder.cs
+++ Assets/Scripts/System/CutScene/TimelineBinder.cs
@@ -1,5 +1,4 @@
-﻿using CryptoQuest.Gameplay;
-using IndiGames.Core.EditorTools.Attributes.ReadOnlyAttribute;
+﻿using IndiGames.Core.EditorTools.Attributes.ReadOnlyAttribute;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -8,21 +7,15 @@ namespace CryptoQuest.System.CutScene
     public class TimelineBinder : MonoBehaviour
     {
         [SerializeField] private PlayableDirector _director;
-        [SerializeField] private GameplayBus _gameplayBus;
 
         [SerializeField] private string[] _objectsToBindTags;
         [SerializeField] private string[] _trackNames;
 
         [SerializeField, ReadOnly] private GameObject[] _objectsToBind;
 
-        private void OnEnable()
+        private void Awake()
         {
-            _gameplayBus.HeroSpawned += BindObjects;
-        }
-
-        private void OnDisable()
-        {
-            _gameplayBus.HeroSpawned -= BindObjects;
+            BindObjects();
         }
 
         private void BindObjects()
