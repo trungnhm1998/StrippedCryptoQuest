15abed4b5c472f2a3b1dd410016bfdd2d6a34b7e
Anh Cao <anh.caothilan223@gmail.com>
2023-07-21 12:58:07 +0700

    feat(home-menu): edited namespace

--- Assets/Scripts/UI/Menu/MockData/ElementalMockDataSO.cs
+++ Assets/Scripts/UI/Menu/MockData/ElementalMockDataSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace CryptoQuest.MockData
+namespace CryptoQuest.UI.Menu.MockData
 {
     [CreateAssetMenu(menuName = "Gameplay/MockData/Elemental")]
     public class ElementalMockDataSO : ScriptableObject
--- Assets/Scripts/UI/Menu/UIHomeMenu.cs
+++ Assets/Scripts/UI/Menu/UIHomeMenu.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using CryptoQuest.MockData;
+using CryptoQuest.UI.Menu.MockData;
 using UnityEngine;
 using UnityEngine.UI;
 
