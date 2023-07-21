7d9583f0a0f997398405854ec0c0d1301deda173
datdiep754129 <dat.diep@indigames.net>
2023-07-21 14:57:50 +0700

    fix: resolved namespace

--- Assets/Scripts/Gameplay/Battle/ScreenColorTransitionSO.cs
+++ Assets/Scripts/Gameplay/Battle/ScreenColorTransitionSO.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace CryptoQuest
+namespace CryptoQuest.Gameplay.Battle
 {
-    [CreateAssetMenu(menuName = "Crypto Quest/Events/Screen Transition")]
+    [CreateAssetMenu(menuName = "Crypto Quest/Battle/Screen Color Transition")]
     public class ScreenColorTransitionSO : ScriptableObject
     {
         public Color ColorLoadingScreen;
