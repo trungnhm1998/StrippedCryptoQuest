3d90a0c48635d4a6cb29e1d3420c7639d8419886
trungnhm1998 <trung.nguyenhoangminh1998@gmail.com>
2023-07-27 21:02:45 +0700

    feat(cutscene-system): chose yarn node tool

---
+++ Assets/CryptoQuestEditor/Scripts/System/CutsceneSystem/CustomTimelineTracks/YarnSpinnerNodeControlTrack/YarnSpinnerNodePlayableAssetPropertyDrawer.cs
@@ -0,0 +1,12 @@
+﻿using CryptoQuest.System.CutsceneSystem.CustomTimelineTracks.YarnSpinnerNodeControlTrack;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace CryptoQuestEditor.System.CutsceneSystem.CustomTimelineTracks.YarnSpinnerNodeControlTrack
+{
+    [CustomPropertyDrawer(typeof(YarnSpinnerNodePlayableAsset))]
+    public class YarnSpinnerNodePlayableAssetPropertyDrawer : PropertyDrawer
+    {
+
+    }
+}
