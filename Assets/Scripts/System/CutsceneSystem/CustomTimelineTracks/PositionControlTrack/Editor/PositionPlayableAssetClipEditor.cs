0193ccd36e78a29676c0eaf7a9da8997d56ebfb3
trungnhm1998 <trung.nguyenhoangminh1998@gmail.com>
2023-07-29 23:31:13 +0700

    feat(cutscene-system): custom position track

---
+++ Assets/Scripts/System/CutsceneSystem/CustomTimelineTracks/PositionControlTrack/Editor/PositionPlayableAssetClipEditor.cs
@@ -0,0 +1,17 @@
+﻿using CryptoQuest.Timeline.Position;
+using UnityEditor.Timeline;
+using UnityEngine.Timeline;
+
+namespace CryptoQuestEditor.Timeline.Position
+{
+    [CustomTimelineEditor(typeof(PositionClip))]
+    public class PositionPlayableAssetClipEditor : ClipEditor
+    {
+        public override void OnClipChanged(TimelineClip clip)
+        {
+            var positionPlayableAsset = clip.asset as PositionClip;
+            if (positionPlayableAsset != null)
+                clip.displayName = positionPlayableAsset.Position.ToString();
+        }
+    }
+}
