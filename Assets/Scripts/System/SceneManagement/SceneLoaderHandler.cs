728d7d5ffe5aaf3f54d795c67bbe1e7af05a372c
CA-INDI-0045\minh <minh.nguyen@indigames.net>
2023-08-02 18:40:02 +0700

    feat: add load additive scene with spiral

--- Assets/Scripts/System/SceneManagement/SceneLoaderHandler.cs
+++ Assets/Scripts/System/SceneManagement/SceneLoaderHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using CryptoQuest.UI.SpiralFX;
+using IndiGames.Core.Events.ScriptableObjects;
 using IndiGames.Core.SceneManagementSystem;
 using IndiGames.Core.SceneManagementSystem.Events.ScriptableObjects;
 using IndiGames.Core.SceneManagementSystem.ScriptableObjects;
@@ -15,6 +16,8 @@ namespace CryptoQuest.System.SceneManagement
     {
         [SerializeField] private SceneLoaderBus _sceneLoadBus;
         [SerializeField] private LoadSceneEventChannelSO _loadMapWithSpiralEventChannel;
+        [SerializeField] private LoadSceneEventChannelSO _loadAdditiveWithSpiralEventChannel;
+        [SerializeField] private VoidEventChannelSO _AdditiveSceneLoadedWithSpiralFX;
         [SerializeField] private SpiralConfigSO _spiralConfigSO;
         public SceneScriptableObject CurrentLoadedScene => _currentLoadedScene;
 
@@ -27,12 +30,14 @@ namespace CryptoQuest.System.SceneManagement
         {
             base.OnEnable();
             _loadMapWithSpiralEventChannel.LoadingRequested += MapLoadingRequestedWithSpiralFX;
+            _loadAdditiveWithSpiralEventChannel.LoadingRequested += AdditiveSceneLoadingWithSpiralRequested;
         }
 
         protected override void OnDisable()
         {
             base.OnDisable();
             _loadMapWithSpiralEventChannel.LoadingRequested -= MapLoadingRequestedWithSpiralFX;
+            _loadAdditiveWithSpiralEventChannel.LoadingRequested -= AdditiveSceneLoadingWithSpiralRequested;
         }
 
         private void MapLoadingRequestedWithSpiralFX(SceneScriptableObject locationToLoad)
@@ -52,6 +57,34 @@ namespace CryptoQuest.System.SceneManagement
             {
                 UnloadPreviousSceneWithSpiralFX();
             }
+        }
+
+        private void AdditiveSceneLoadingWithSpiralRequested(SceneScriptableObject sceneToLoad)
+        {
+            if (_isLoading) return;
+
+            _sceneToLoad = sceneToLoad;
+            _isLoading = true;
+
+            if (!_gameplayManagerSceneInstance.Scene.isLoaded &&
+                !_gameplayManagerSceneSO.SceneReference.OperationHandle.IsValid())
+            {
+                _gameplayManagerLoadingOperationHandle =
+                    _gameplayManagerSceneSO.SceneReference.LoadSceneAsync(LoadSceneMode.Additive);
+                _gameplayManagerLoadingOperationHandle.Completed += GameplayManagerSceneLoaded;
+                return;
+            }
+
+            StartCoroutine(LoadAdditiveNewSceneWithSpiral());
+        }
+
+        private IEnumerator LoadAdditiveNewSceneWithSpiral()
+        {
+            _spiralConfigSO.OnSpiralIn();
+            yield return new WaitForSeconds(_spiralConfigSO.Duration);
+            _sceneLoadingOperationHandle =
+                _sceneToLoad.SceneReference.LoadSceneAsync(LoadSceneMode.Additive, true, 0);
+            _sceneLoadingOperationHandle.Completed += NewAdditiveSceneLoadedWithSpiralFX;
         }
 
         private void UnloadPreviousSceneWithSpiralFX()
@@ -84,6 +117,21 @@ namespace CryptoQuest.System.SceneManagement
             _sceneLoadingOperationHandle.Completed += NewSceneLoadedWithSpiralFX;
         }
 
+        private void NewAdditiveSceneLoadedWithSpiralFX(AsyncOperationHandle<SceneInstance> asyncOpSceneInstance)
+        {
+            var scene = asyncOpSceneInstance.Result.Scene;
+            SceneManager.SetActiveScene(scene);
+
+            _isLoading = false;
+            StartCoroutine(OnAdditiveSceneLoaded());
+        }
+
+        private IEnumerator OnAdditiveSceneLoaded()
+        {
+            _spiralConfigSO.OnSpiralOut();
+            yield return new WaitForSeconds(_spiralConfigSO.Duration);
+            _AdditiveSceneLoadedWithSpiralFX.RaiseEvent();
+        }
 
         private void NewSceneLoadedWithSpiralFX(AsyncOperationHandle<SceneInstance> asyncOpSceneInstance)
         {
