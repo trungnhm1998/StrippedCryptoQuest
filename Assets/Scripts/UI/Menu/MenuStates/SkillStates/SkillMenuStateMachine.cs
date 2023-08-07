33d5e49abc2cf94fa0c081026229f7bbb7ae3e0a
Anh Cao <anh.caothilan223@gmail.com>
2023-08-07 16:46:50 +0700

    feat(skill-menu): could open skill menu now

---
+++ Assets/Scripts/UI/Menu/MenuStates/SkillStates/SkillMenuStateMachine.cs
@@ -0,0 +1,28 @@
+﻿using CryptoQuest.UI.Menu.MenuStates.SkillStates;
+using CryptoQuest.UI.Menu.Panels.Skill;
+using UnityEngine;
+
+namespace CryptoQuest.UI.Menu.MenuStates.SkillStates
+{
+    public class SkillMenuStateMachine : MenuStateMachine
+    {
+        public static readonly string NavStatus = "NavStatus";
+
+        private new readonly UISkillMenu _panel;
+        
+        /// <summary>
+        /// Setup the state machine for status menu.
+        /// </summary>
+        /// <param name="panel"></param>
+        public SkillMenuStateMachine(UISkillMenu panel) : base(panel)
+        {
+            // Could create a factory here if new keyword becomes a problem.
+            AddState(NavStatus, new UnFocusSkillState(panel));
+            // AddState(Status, new FocusStatusState(panel));
+            // AddState(Equipment, new EquipmentState(panel));
+            // AddState(EquipmentSelection, new ChangeEquipmentState(panel));
+            //
+            // SetStartState(Equipment);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Menu/Panels/Skill/UISkillMenu.cs
@@ -0,0 +1,25 @@
+﻿using CryptoQuest.UI.Menu.MenuStates.SkillStates;
+using FSM;
+using UnityEngine;
+
+namespace CryptoQuest.UI.Menu.Panels.Skill
+{
+    /// <summary>
+    /// The context that hopefully holds all the UI information for the Status Menu. This is a mono behaviour class that
+    /// can controls all the UI element or at least delegate back the reference to the correct state when needed.
+    /// </summary>
+    public class UISkillMenu : UIMenuPanel
+    {
+        /// <summary>
+        /// Return the specific state machine for this panel.
+        /// </summary>
+        /// <param name="menuManager"></param>
+        /// <returns>The <see cref="SkillMenuStateMachine"/> which derived
+        /// <see cref="CryptoQuest.UI.Menu.MenuStates.MenuStateMachine"/> derived
+        /// from <see cref="StateMachine"/> which also derived from <see cref="StateBase"/></returns>
+        public override StateBase<string> GetPanelState(MenuManager menuManager)
+        {
+            return new SkillMenuStateMachine(this);
+        }
+    }
+}
