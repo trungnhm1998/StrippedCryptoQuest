e23fd025d72d4b3b9462a5dd40c2be5dd10f778b
trungnhm1998 <trung.nguyenhoangminh1998@gmail.com>
2023-08-15 16:04:14 +0700

    fix(menu): rebuild nav hint when change language

---
+++ Assets/Scripts/UI/Common/RebuildLayout.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CryptoQuest.UI.Common
+{
+    public class RebuildLayout : MonoBehaviour
+    {
+        public RectTransform rectTransform;
+
+        public void Rebuild()
+        {
+            LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
+        }
+    }
+}
