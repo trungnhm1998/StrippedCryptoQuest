520d20b992b8ffd274e849a96e5a910f385816a5
datdiep754129 <104046069+datdiep754129@users.noreply.github.com>
2023-08-23 10:46:05 +0700

    refactor: npc prefab
    * refactor: rename npc folder & refactor npc prefab
    
    * refactor: resolved pull request
    
    * refactor: resolved pull request

--- Assets/Scripts/Character/NpcMovement.cs
+++ Assets/Scripts/Character/NpcMovement.cs
@@ -5,6 +5,7 @@ namespace CryptoQuest.Character
 {
     public class NpcMovement : CharacterBehaviour
     {
+        [SerializeField] private GameObject _characterObject;
         [SerializeField] private Transform _pointAPosition;
         [SerializeField] private Transform _pointBPosition;
         [SerializeField] private Transform _destination;
@@ -14,15 +15,15 @@ namespace CryptoQuest.Character
         private IFacingStrategy facingStrategy = new NpcFacingStrategy();
         private float _currentSpeed;
         private float _timeLeftUntilWaitingFinished;
+        private Vector3 _characterLocalPosition;
 
         void Awake()
         {
             if (_destination == null) _destination = _pointAPosition;
             IsWalking = true;
-            transform.position = _destination.position;
+            _characterObject.transform.position = _destination.position;
             _currentSpeed = _speed;
             _timeLeftUntilWaitingFinished = _waitBeforeMovingAgainTime;
-            SetFacingDirection(_destination.localPosition - gameObject.transform.localPosition);
         }
 
         void Update()
@@ -32,8 +33,9 @@ namespace CryptoQuest.Character
 
         private void Move()
         {
-            transform.localPosition = Vector2.MoveTowards(transform.localPosition, _destination.localPosition, _currentSpeed * Time.deltaTime);
-            if ((_destination.localPosition - transform.localPosition).magnitude < _minDistanceToChangeDestination)
+            _characterLocalPosition = _characterObject.transform.localPosition;
+            _characterObject.transform.localPosition = Vector2.MoveTowards(_characterLocalPosition, _destination.localPosition, _currentSpeed * Time.deltaTime);
+            if ((_destination.localPosition - _characterLocalPosition).magnitude < _minDistanceToChangeDestination)
             {
                 IsWalking = false;
                 _currentSpeed = 0f;
@@ -58,22 +60,24 @@ namespace CryptoQuest.Character
                 _destination = _pointAPosition;
             }
             _currentSpeed = _speed;
-            SetFacingDirection(_destination.localPosition - gameObject.transform.localPosition);
+            SetFacingDirection(_destination.localPosition - _characterLocalPosition);
         }
+        
         private void OnTriggerStay2D(Collider2D other)
         {
             if (other.CompareTag("Player"))
             {
                 IsWalking = false;
                 _currentSpeed = 0f;
-                SetFacingDirection(facingStrategy.Execute(transform.position, other.transform.position));
+                SetFacingDirection(facingStrategy.Execute(_characterObject.transform.position, other.transform.position));
             }
         }
+
         private void OnTriggerExit2D(Collider2D other)
         {
             IsWalking = true;
             _currentSpeed = _speed;
-            SetFacingDirection(_destination.localPosition - gameObject.transform.localPosition);
+            SetFacingDirection(_destination.localPosition - _characterLocalPosition);
         }
     }
 }
