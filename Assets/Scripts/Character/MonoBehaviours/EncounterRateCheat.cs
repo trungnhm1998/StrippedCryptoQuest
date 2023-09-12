2929caf5c2baee3fd23ab4171e0214672305f85d
trungnhm1998 <trung.nguyenhoangminh1998@gmail.com>
2023-09-12 19:44:27 +0700

    feat(cheat): distance until count as steps for encounter rate

--- Assets/Scripts/Character/MonoBehaviours/EncounterRateCheat.cs
+++ Assets/Scripts/Character/MonoBehaviours/EncounterRateCheat.cs
@@ -1,9 +1,22 @@
-﻿using UnityEngine;
+﻿using CommandTerminal;
+using CryptoQuest.System.Cheat;
+using UnityEngine;
 
 namespace CryptoQuest.Character.MonoBehaviours
 {
-    public class EncounterRateCheat : MonoBehaviour
+    public class EncounterRateCheat : MonoBehaviour, ICheatInitializer
     {
-        
+        public void InitCheats()
+        {
+            Terminal.Shell.AddCommand("enr", SetEncounterRate, 1, 1,
+                "Set encounter rate as distance per step, the higher the number the less likely to encounter");
+        }
+
+        private void SetEncounterRate(CommandArg[] obj)
+        {
+            var rate = obj[0].Float;
+            var hero = FindObjectOfType<HeroController>(); // it is a cheat I don't care about performance
+            hero.SetDistanceTilNextStep(rate);
+        }
     }
 }
