288bb2fd5517a4b93fbf3311fd688436e75b1b70
Long18 <us.thanhlong18@gmail.com>
2023-09-13 14:53:24 +0700

    fix: event not raise when value changed

---
+++ Assets/Scripts/Character/Attributes/LoggerAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using IndiGames.GameplayAbilitySystem.AttributeSystem;
+using IndiGames.GameplayAbilitySystem.AttributeSystem.Components;
+using IndiGames.GameplayAbilitySystem.AttributeSystem.ScriptableObjects;
+using UnityEngine;
+using CoreAttribute = IndiGames.GameplayAbilitySystem.AttributeSystem.ScriptableObjects.AttributeScriptableObject;
+
+namespace CryptoQuest.Character.Attributes
+{
+    public class LoggerAttribute : AttributesEventBase
+    {
+        public Action<AttributeSystemBehaviour, AttributeValue> OnAttributeChanged;
+
+        public override void PreAttributeChange(AttributeSystemBehaviour attributeSystem,
+            ref AttributeValue newAttributeValue)
+        {
+            OnAttributeChanged?.Invoke(attributeSystem, newAttributeValue);
+        }
+
+
+        public override void PostAttributeChange(AttributeSystemBehaviour attributeSystem,
+            ref AttributeValue oldAttributeValue,
+            ref AttributeValue newAttributeValue) { }
+    }
+}
