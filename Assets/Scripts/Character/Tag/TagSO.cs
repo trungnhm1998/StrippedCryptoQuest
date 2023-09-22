07aa063744a57b6eae9a2e4b3720bc3387b2cda5
Long18 <us.thanhlong18@gmail.com>
2023-09-22 15:24:01 +0700

    feat(tag): create tag with image

---
+++ Assets/Scripts/Character/Tag/TagSO.cs
@@ -0,0 +1,12 @@
+﻿using IndiGames.GameplayAbilitySystem.TagSystem.ScriptableObjects;
+using UnityEngine;
+
+namespace CryptoQuest.Character.Tag
+{
+    
+    [CreateAssetMenu(menuName = "Crypto Quest/Character/Tag", fileName = "Create Tag", order = 0)]
+    public class TagSO : TagScriptableObject
+    {
+        [field: SerializeField] public Sprite Icon { get; private set; }
+    }
+}
