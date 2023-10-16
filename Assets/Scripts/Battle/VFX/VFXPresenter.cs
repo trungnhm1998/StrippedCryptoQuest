e330e0fa550f869c6594e8b3681a481d93dbb97d
Huy Truong <109120616+HuyTruongIndi@users.noreply.github.com>
2023-10-16 14:54:00 +0700

    build: broken namespace (#1872)

--- Assets/Scripts/Battle/VFX/VFXPresenter.cs
+++ Assets/Scripts/Battle/VFX/VFXPresenter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CryptoQuest.Battle.Components;
-using CryptoQuest.Character.Ability;
+using CryptoQuest.AbilitySystem.Abilities;
 using CryptoQuest.Battle.Events;
 using TinyMessenger;
 
