604aaa7983d9fa0e13030e89b31b863cae292466
Huy Truong <109120616+HuyTruongIndi@users.noreply.github.com>
2023-10-17 15:54:59 +0700

    feat: decrease gold and restore character stats when lose battle (#1900)

--- Assets/Scripts/Battle/LostBattleHandler.cs
+++ Assets/Scripts/Battle/LostBattleHandler.cs
@@ -1,5 +1,10 @@
-﻿using CryptoQuest.Battle.Events;
+﻿using CryptoQuest.AbilitySystem.Attributes;
+using CryptoQuest.Battle.Components;
+using CryptoQuest.Battle.Events;
 using CryptoQuest.Gameplay.Encounter;
+using CryptoQuest.Gameplay.Inventory;
+using CryptoQuest.Gameplay.PlayerParty;
+using CryptoQuest.System;
 using IndiGames.Core.SceneManagementSystem;
 using IndiGames.Core.SceneManagementSystem.ScriptableObjects;
 using TinyMessenger;
@@ -10,6 +15,8 @@ namespace CryptoQuest.Battle
     public class LostBattleHandler : PostBattleManager
     {
         [SerializeField] private BattleResultEventSO _battleCompletedEvent;
+        [SerializeField] private int _divideGold = 2;
+        [SerializeField] private AttributeWithMaxCapped[] _resetAttributes;
         private TinyMessageSubscriptionToken _lostToken;
 
         private void Awake()
@@ -27,6 +34,8 @@ namespace CryptoQuest.Battle
         private void HandleLost(BattleLostEvent lostContext)
         {
             _context = lostContext;
+            DecreaseGold();
+            RestoreCharacter();
             AdditiveGameSceneLoader.SceneUnloaded += TeleportToClosestTownAfterSceneUnloaded;
             UnloadBattleScene();
             NotifyBattleResult(_context.Battlefield);
@@ -48,6 +57,39 @@ namespace CryptoQuest.Battle
                 Battlefield = battlefield
             };
             _battleCompletedEvent.RaiseEvent(result);
+        }
+
+        private void DecreaseGold()
+        {
+            var inventoryController = ServiceProvider.GetService<IInventoryController>();
+            var wallet = inventoryController.Inventory.WalletController.Wallet;
+
+            var newGold = Mathf.FloorToInt(wallet.Gold.Amount / _divideGold);
+            wallet.Gold.SetCurrencyAmount(newGold);
+        }
+
+        private void RestoreCharacter()
+        {
+            var party = ServiceProvider.GetService<IPartyController>();
+
+            foreach (var character in party.Slots)
+            {
+                if (character.IsValid())
+                {
+                    ResetAttributeValue(character.HeroBehaviour);
+                }
+            }
+        }
+
+        private void ResetAttributeValue(HeroBehaviour hero)
+        {
+            for (int i = 0; i < _resetAttributes.Length; i++)
+            {
+                var attributeToReset = _resetAttributes[i];
+                if (!hero.AttributeSystem.TryGetAttributeValue(attributeToReset.CappedAttribute,
+                        out var cappedAttributeValue)) continue;
+                hero.AttributeSystem.SetAttributeBaseValue(attributeToReset, cappedAttributeValue.CurrentValue);
+            }
         }
     }
 }
