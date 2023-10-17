a9b3451eb0f72b823eef52dbd94a0179a9af684a
Thành Long (Morningstar WiIIiam) <us.thanhlong18@gmail.com>
2023-10-17 11:04:10 +0700

    feat(cheat): game timescale (#1891)

---
+++ Assets/Scripts/System/Cheat/SystemCheat.cs
@@ -0,0 +1,22 @@
+﻿using CommandTerminal;
+using UnityEngine;
+
+namespace CryptoQuest.System.Cheat
+{
+    public class SystemCheat : MonoBehaviour, ICheatInitializer
+    {
+        public void InitCheats()
+        {
+            Terminal.Shell.AddCommand("speed", TriggerModifySpeedValue, 1, 1,
+                "speed <new_value>, to modify current speed value of game");
+        }
+
+        private void TriggerModifySpeedValue(CommandArg[] obj)
+        {
+            var newSpeed = obj[0].Float;
+            Time.timeScale = newSpeed;
+
+            Debug.Log($"Updated game speed to {newSpeed}");
+        }
+    }
+}
