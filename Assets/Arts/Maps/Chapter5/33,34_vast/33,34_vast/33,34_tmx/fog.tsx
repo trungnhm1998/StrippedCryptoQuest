753e464ddc7f07848b2f91852d214fea6ddca779
yuziyosida <136284660+yuziyosida@users.noreply.github.com>
2023-10-18 19:36:21 +0900

    feat: create cutscene chapter 5 (#1703)

---
+++ Assets/Arts/Maps/Chapter5/33,34_vast/33,34_vast/33,34_tmx/fog.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="fog" tilewidth="32" tileheight="32" tilecount="110" columns="11">
+ <image source="../../../../World_Map/0_png/fog.png" width="374" height="332"/>
+</tileset>
