4043ba87b1abd180ab39c0dbb3de3160f288efc3
trungnhm1998 <trung.nguyenhoangminh1998@gmail.com>
2023-10-19 18:02:22 +0700

    fix: remove effects when battle is unloading

---
+++ Assets/Scripts/Battle/UpdatePlayerPartyStatsPostBattle.cs
@@ -0,0 +1,34 @@
+﻿using CryptoQuest.Battle.Events;
+using CryptoQuest.Gameplay.PlayerParty;
+using CryptoQuest.System;
+using TinyMessenger;
+using UnityEngine;
+
+namespace CryptoQuest.Battle
+{
+    public class UpdatePlayerPartyStatsPostBattle : MonoBehaviour
+    {
+        private TinyMessageSubscriptionToken _unloadingEvent;
+
+        private void Awake()
+        {
+            _unloadingEvent = BattleEventBus.SubscribeEvent<UnloadingEvent>(UpdatePartyStats);
+        }
+
+        private void OnDestroy()
+        {
+            BattleEventBus.UnsubscribeEvent(_unloadingEvent);
+        }
+
+        private void UpdatePartyStats(UnloadingEvent ctx)
+        {
+            var partyController = ServiceProvider.GetService<IPartyController>();
+            foreach (var slot in partyController.Slots)
+            {
+                if (slot.IsValid() == false) continue;
+                // this will also remove expired effects and update the attribute values
+                slot.HeroBehaviour.GameplayEffectSystem.UpdateAttributeModifiersUsingAppliedEffects();
+            }
+        }
+    }
+}
