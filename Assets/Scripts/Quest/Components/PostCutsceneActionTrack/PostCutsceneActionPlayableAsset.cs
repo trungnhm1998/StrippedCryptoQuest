e5012111854f3a1fdb1677057e61852bc9258758
minhnguyen0699 <102936052+minhnguyen0699@users.noreply.github.com>
2023-10-19 16:32:04 +0700

    chore: clean up (#1955)

--- Assets/Scripts/Quest/Components/PostCutsceneActionTrack/PostCutsceneActionPlayableAsset.cs
+++ Assets/Scripts/Quest/Components/PostCutsceneActionTrack/PostCutsceneActionPlayableAsset.cs
@@ -11,14 +11,12 @@ namespace CryptoQuest.System.CutsceneSystem.CustomTimelineTracks
     [Serializable]
     public class PostCutsceneActionPlayableAsset : PlayableAsset, ITimelineClipAsset
     {
-        [SerializeField] private PlayableAsset _playableAsset;
         [SerializeField] private PostCutsceneActionPlayableBehaviour _template;
         public ClipCaps clipCaps => ClipCaps.None;
 
         public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
         {
             var playable = ScriptPlayable<PostCutsceneActionPlayableBehaviour>.Create(graph, _template);
-            var director = owner.GetComponent<PlayableDirector>();
 
             return playable;
         }
