a2486293c00feacd603247847cb4b7df4d3f2011
datdiep754129 <104046069+datdiep754129@users.noreply.github.com>
2023-10-24 10:30:02 +0700

    fix: can not interact with npc (#2032)

--- Assets/Scripts/Character/MonoBehaviours/ZoneTriggerController.cs
+++ Assets/Scripts/Character/MonoBehaviours/ZoneTriggerController.cs
@@ -7,10 +7,11 @@ namespace CryptoQuest.Character.MonoBehaviours
     {
         [SerializeField] private UnityEvent<bool, GameObject> _enterZone = default;
         [SerializeField] private LayerMask _layers = default;
+        public static readonly string Tag = "NPC";
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if ((1 << other.gameObject.layer & _layers) != 0)
+            if ((1 << other.gameObject.layer & _layers) != 0 && other.tag == Tag)
             {
                 _enterZone.Invoke(true, other.gameObject);
             }
@@ -18,7 +19,7 @@ namespace CryptoQuest.Character.MonoBehaviours
 
         private void OnTriggerExit2D(Collider2D other)
         {
-            if ((1 << other.gameObject.layer & _layers) != 0)
+            if ((1 << other.gameObject.layer & _layers) != 0 && other.tag == Tag)
             {
                 _enterZone.Invoke(false, other.gameObject);
             }
