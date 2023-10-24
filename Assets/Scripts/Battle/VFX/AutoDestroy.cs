ad7ea9d752d17743fc57ee56ac2d4d7e33265807
trungnhm1998 <trung.nguyenhoangminh1998@gmail.com>
2023-10-24 10:55:45 +0700

    fix: vfx not destroy

---
+++ Assets/Scripts/Battle/VFX/AutoDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace CryptoQuest.Battle.VFX
+{
+    public class AutoDestroy : MonoBehaviour
+    {
+        [SerializeField] private float _delay = 2.1f;
+
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(_delay);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Battle/VFX/MoveTowardDirection.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CryptoQuest.Battle.VFX
+{
+    public class MoveTowardDirection : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _direction;
+        
+        private void Update()
+        {
+            transform.position += _direction * Time.deltaTime;
+        }
+    }
+}
