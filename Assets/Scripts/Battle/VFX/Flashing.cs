aa4610fed1e1b2d190fcb623a6f7efa3b4e46afd
trungnhm1998 <trung.nguyenhoangminh1998@gmail.com>
2023-10-24 11:33:36 +0700

    feat: vfx behaviours
    - flashing
    - spawn off set

---
+++ Assets/Scripts/Battle/VFX/Flashing.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace CryptoQuest.Battle.VFX
+{
+    public class Flashing : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        private void OnValidate() => _spriteRenderer = GetComponent<SpriteRenderer>();
+        private void Update() => _spriteRenderer.color = Color.Lerp(_spriteRenderer.color, Color.clear, Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Battle/VFX/SpawnOffset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace CryptoQuest.Battle.VFX
+{
+    public class SpawnOffset : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _offset = new Vector3(0, 0.2f, 0);
+
+        private void Start() => transform.position += _offset;
+    }
+}
