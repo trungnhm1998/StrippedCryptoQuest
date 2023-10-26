149d652e8f01dbae7624f32fa311832a8be91b73
thai.phi <thai.phi@indigames.net>
2023-10-26 11:04:12 +0700

    fix: Interactive Quest not triggered in Tarvern

--- Assets/Scripts/Quest/Controllers/BaseQuestController.cs
+++ Assets/Scripts/Quest/Controllers/BaseQuestController.cs
@@ -7,7 +7,7 @@ namespace CryptoQuest.Quest.Controllers
     public abstract class BaseQuestController : MonoBehaviour
     {
         protected QuestManager _questManager;
-        private void Awake() => _questManager = GetService<QuestManager>();
+        private void Start() => _questManager = GetService<QuestManager>();
 
         protected abstract void OnQuestFinish();
     }
