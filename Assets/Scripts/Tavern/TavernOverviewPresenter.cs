5043d231989aa0798b54fedebc8cefd62fef85f7
Anh Cao <anh.cao@indigames.net>
2023-10-27 18:39:26 +0700

    feat(tavern): interact with tavern lady to use nft-related feature

---
+++ Assets/Scripts/Tavern/TavernOverviewPresenter.cs
@@ -0,0 +1,11 @@
+﻿using CryptoQuest.Tavern.UI;
+using UnityEngine;
+
+namespace CryptoQuest.Tavern
+{
+    public class TavernOverviewPresenter : MonoBehaviour
+    {
+        [SerializeField] private UIOverview _tavernUiOverview;
+        public UIOverview TavernUiOverview => _tavernUiOverview;
+    }
+}
---
+++ Assets/Scripts/Tavern/TavernSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CryptoQuest.Tavern.ScriptableObjects;
+using UnityEngine;
+
+namespace CryptoQuest.Tavern
+{
+    public class TavernSystem : MonoBehaviour
+    {
+        [SerializeField] private ShowTavernEventChannelSO _showTavern;
+        [SerializeField] private GameObject _tavernOverviewPresenter;
+
+        private void OnEnable()
+        {
+            _showTavern.EventRaised += ShowTavernRequested;
+        }
+
+        private void OnDisable()
+        {
+            _showTavern.EventRaised -= ShowTavernRequested;
+        }
+
+        private void ShowTavernRequested()
+        {
+            _tavernOverviewPresenter.SetActive(true);
+        }
+    }
+}
