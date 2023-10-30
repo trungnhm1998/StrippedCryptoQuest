dce12314a68c8bf23da5930730648e1441486fcd
Anh Cao <anh.cao@indigames.net>
2023-10-30 17:52:25 +0700

    feat(tavern): character-replacement ui & flow to enter/exit

---
+++ Assets/Scripts/Tavern/States/SelectCharacterState.cs
@@ -0,0 +1,39 @@
+﻿using CryptoQuest.Tavern.UI;
+using UnityEngine;
+
+namespace CryptoQuest.Tavern.States
+{
+    public class SelectCharacterState : StateMachineBehaviour
+    {
+        private Animator _animator;
+        private TavernPresenter _presenter;
+
+        private static readonly int TurnBack = Animator.StringToHash("isTurnBack");
+
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo,
+            int layerIndex)
+        {
+            _animator = animator;
+
+            _presenter = animator.GetComponent<TavernPresenter>();
+            _presenter.UICharacterReplacement.gameObject.SetActive(true);
+            
+            _presenter.TavernInputManager.CancelEvent += CancelCharacterReplacement;
+
+        }
+
+        private void CancelCharacterReplacement()
+        {
+            _presenter.UICharacterReplacement.gameObject.SetActive(false);
+            _animator.SetTrigger(TurnBack);
+
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo,
+            int layerIndex)
+        {
+            _presenter.TavernInputManager.CancelEvent -= CancelCharacterReplacement;
+        }
+    }
+}
---
+++ Assets/Scripts/Tavern/UI/CharacterReplacement/UICharacterReplacement.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using UnityEngine;
+
+namespace CryptoQuest.Tavern.UI.CharacterReplacement
+{
+    public class UICharacterReplacement : MonoBehaviour { }
+}
