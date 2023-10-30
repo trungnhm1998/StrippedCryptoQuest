d5ae745119440dcfd8a8e935fad2cfa97747bea8
Anh Cao <anh.cao@indigames.net>
2023-10-30 16:25:39 +0700

    feat(tavern): input system

---
+++ Assets/Scripts/Tavern/TavernInputManager.cs
@@ -0,0 +1,53 @@
+﻿using CryptoQuest.Input;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace CryptoQuest.Tavern
+{
+    public class TavernInputManager : MonoBehaviour, InputActions.ITavernActions
+    {
+        [SerializeField] private InputMediatorSO _inputMediatorSO;
+
+        public event UnityAction CancelEvent;
+        public event UnityAction SubmitEvent;
+        public event UnityAction<Vector2> NavigateEvent;
+
+        private void OnEnable()
+        {
+            _inputMediatorSO.InputActions.Tavern.SetCallbacks(this);
+        }
+
+        private void OnDisable()
+        {
+            _inputMediatorSO.InputActions.Tavern.RemoveCallbacks(this);
+        }
+
+        public void EnableInput()
+        {
+            _inputMediatorSO.DisableAllInput();
+            _inputMediatorSO.InputActions.Tavern.Enable();
+        }
+
+        public void DisableInput()
+        {
+            _inputMediatorSO.InputActions.Tavern.Disable();
+            _inputMediatorSO.EnableMapGameplayInput();
+        }
+
+        public void OnCancel(InputAction.CallbackContext context)
+        {
+            if (context.performed) CancelEvent?.Invoke();
+        }
+
+        public void OnNavigate(InputAction.CallbackContext context)
+        {
+            if (context.performed) NavigateEvent?.Invoke(context.ReadValue<Vector2>());
+        }
+
+        public void OnSubmit(InputAction.CallbackContext context)
+        {
+            if (context.performed) SubmitEvent?.Invoke();
+        }
+    }
+}
--- Assets/Scripts/Tavern/UI/UIOverview.cs
+++ Assets/Scripts/Tavern/UI/UIOverview.cs
@@ -1,6 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace CryptoQuest.Tavern.UI
 {
-    public class UIOverview : MonoBehaviour { }
+    public class UIOverview : MonoBehaviour
+    {
+        public event UnityAction CharacterReplacementButtonPressedEvent;
+        public event UnityAction PartyOrganizationButtonPressedEvent;
+
+        [SerializeField] private Button _defaultSelectButton;
+
+        private void OnEnable()
+        {
+            _defaultSelectButton.Select();
+        }
+
+        public void CharacterReplacementButtonPressed()
+        {
+            CharacterReplacementButtonPressedEvent?.Invoke();
+        }
+
+        public void PartyOrganizationButtonPressed()
+        {
+            PartyOrganizationButtonPressedEvent?.Invoke();
+        }
+    }
 }
