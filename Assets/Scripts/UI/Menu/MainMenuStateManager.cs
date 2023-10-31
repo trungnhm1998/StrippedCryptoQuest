3106d517374ef25c4e44c774281e5900cce4c743
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-10-31 20:44:34 +0700

    refactor!: main menu tab navigator

---
+++ Assets/Scripts/UI/Menu/MainMenuStateManager.cs
@@ -0,0 +1,44 @@
+﻿using CryptoQuest.Input;
+using IndiGames.Core.Events.ScriptableObjects;
+using UnityEngine;
+
+namespace CryptoQuest.UI.Menu
+{
+    /// <summary>
+    /// Management show and hide main menu state based on input
+    /// </summary>
+    public class MainMenuStateManager : MonoBehaviour
+    {
+        [SerializeField] private VoidEventChannelSO _forceCloseMainMenuEvent;
+        [SerializeField] private InputMediatorSO _input;
+        [SerializeField] private UIMainMenu _uiMainMenuPanel;
+
+        private void OnEnable()
+        {
+            _input.ShowMainMenuEvent += OpenMainMenu;
+        }
+
+        private void OnDisable()
+        {
+            _input.ShowMainMenuEvent -= OpenMainMenu;
+            _input.CloseMainMenuEvent -= CloseMainMenu;
+            _forceCloseMainMenuEvent.EventRaised -= CloseMainMenu;
+        }
+
+        private void OpenMainMenu()
+        {
+            _input.CloseMainMenuEvent += CloseMainMenu;
+            _forceCloseMainMenuEvent.EventRaised += CloseMainMenu;
+            _uiMainMenuPanel.gameObject.SetActive(true);
+            _input.EnableInputMap("Menus");
+        }
+
+        private void CloseMainMenu()
+        {
+            _input.CloseMainMenuEvent -= CloseMainMenu;
+            _forceCloseMainMenuEvent.EventRaised -= CloseMainMenu;
+            _uiMainMenuPanel.gameObject.SetActive(false);
+            _input.EnableInputMap("MapGameplay");
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Menu/UITabFocus.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace CryptoQuest.UI.Menu
+{
+    public class UITabFocus : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private Color _unfocus = Color.gray;
+        [SerializeField] private Color _focus = Color.white;
+
+        public void Focus()
+        {
+            _text.color = _focus;
+        }
+
+        public void UnFocus()
+        {
+            _text.color = _unfocus;
+        }
+    }
+}
