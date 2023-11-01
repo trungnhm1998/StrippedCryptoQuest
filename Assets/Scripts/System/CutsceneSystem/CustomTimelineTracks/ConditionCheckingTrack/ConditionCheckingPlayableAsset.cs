0d5e7b3026e61d1823fc0656d0e4097f0ac59db3
minhnguyen0699 <minh.nguyen@indigames.net>
2023-11-01 18:02:47 +0700

    feat: make interactive cutscene by manipulate playable director time

---
+++ Assets/Scripts/System/CutsceneSystem/CustomTimelineTracks/ConditionCheckingTrack/ConditionCheckingPlayableAsset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CryptoQuest.System.CutsceneSystem.CustomTimelineTracks.FadeTransitionTrack;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace CryptoQuest.System.CutsceneSystem.CustomTimelineTracks.ConditionCheckingTrack
+{
+    [Serializable]
+    public class ConditionCheckingPlayableAsset : PlayableAsset, ITimelineClipAsset
+    {
+        [SerializeField] private ConditionCheckingTrackPlayableBehavior _template;
+        public ClipCaps clipCaps => ClipCaps.None;
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            var playable = ScriptPlayable<ConditionCheckingTrackPlayableBehavior>.Create(graph, _template);
+
+            return playable;
+        }
+    }
+}
