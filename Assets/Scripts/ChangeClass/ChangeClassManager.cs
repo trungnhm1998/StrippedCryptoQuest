bb2be0115401f374dadde3cdcb5b2fbe4d954401
datdiep754129 <104046069+datdiep754129@users.noreply.github.com>
2023-11-01 11:29:37 +0700

    feat: Implement dialog for change classs (#2224)

--- Assets/Scripts/ChangeClass/ChangeClassManager.cs
+++ Assets/Scripts/ChangeClass/ChangeClassManager.cs
@@ -1,40 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using CryptoQuest.ChangeClass.ScriptableObjects;
+using CryptoQuest.UI.Dialogs.Dialogue;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 namespace CryptoQuest.ChangeClass
 {
     public class ChangeClassManager : MonoBehaviour
     {
+        public UnityAction OpenChangeClassEvent;
         [SerializeField] private ShowChangeClassEventChannelSO _showChangeClassEvent;
+        [SerializeField] private ChangeClassDialogController _dialogController;
         [SerializeField] private ChangeClassStateController _stateController;
         [SerializeField] private ChangeClassInputManager _input;
-        [SerializeField] private GameObject _changeClassContent;
+        [SerializeField] private GameObject _changeClassPanel;
+        [SerializeField] private Button _defaultButton;
 
 
         private void OnEnable()
         {
             _showChangeClassEvent.EventRaised += ShowChangeClass;
             _stateController.ExitStateEvent += HideChangeClass;
+            _defaultButton.onClick.AddListener(OnButtonClicked);
         }
 
         private void OnDisable()
         {
             _showChangeClassEvent.EventRaised -= ShowChangeClass;
             _stateController.ExitStateEvent -= HideChangeClass;
+            _defaultButton.onClick.RemoveListener(OnButtonClicked);
         }
 
         private void ShowChangeClass()
         {
-            _changeClassContent.SetActive(true);
+            _dialogController.ShowChangeClassDialog();
+            _defaultButton.Select();
             _input.EnableInput();
+            _changeClassPanel.SetActive(true);
         }
 
         private void HideChangeClass()
         {
-            _changeClassContent.SetActive(false);
+            _changeClassPanel.SetActive(false);
+            _dialogController.HideChangeClassDialog();
             _input.DisableInput();
+        }
+
+        private void OnButtonClicked()
+        {
+            OpenChangeClassEvent?.Invoke();
         }
     }
 }
--- Assets/Scripts/ChangeClass/ChangeClassStateController.cs
+++ Assets/Scripts/ChangeClass/ChangeClassStateController.cs
@@ -5,7 +5,9 @@ namespace CryptoQuest.ChangeClass
 {
     public class ChangeClassStateController : MonoBehaviour
     {
+        [field: SerializeField] public ChangeClassDialogController DialogController { get; private set; }
         [field: SerializeField] public ChangeClassInputManager Input { get; private set; }
+        [field: SerializeField] public ChangeClassManager Manager { get; private set; }
         public UnityAction ExitStateEvent;
     }
 }
