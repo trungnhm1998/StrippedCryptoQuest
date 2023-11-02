90c6c438e36582368692cce2de24598d246773fe
datdiep754129 <dat.diep@indigames.net>
2023-11-02 16:04:52 +0700

    feat: get material from server
    refactor: resolve pull request

--- Assets/Scripts/ChangeClass/ChangeClassPresenter.cs
+++ Assets/Scripts/ChangeClass/ChangeClassPresenter.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using CryptoQuest.Character;
 using CryptoQuest.ChangeClass.View;
 using CryptoQuest.Networking.Menu.DimensionBox;
+using CryptoQuest.ChangeClass.API;
 
 namespace CryptoQuest.ChangeClass
 {
@@ -14,6 +15,7 @@ namespace CryptoQuest.ChangeClass
         [SerializeField] private UIClassToChange _uiClassToChange;
         [SerializeField] private UIClassMaterial _firstClassMaterialPanel;
         [SerializeField] private UIClassMaterial _secondClassMaterialPanel;
+        [SerializeField] private WalletMaterialAPI _materialApi;
         private IWalletCharacterModel _characterModel;
         private List<ICharacterModel> _characterData = new();
         private UIOccupation _material;
@@ -27,15 +29,24 @@ namespace CryptoQuest.ChangeClass
         private void Init()
         {
             _characterModel = GetComponent<IWalletCharacterModel>();
-            StartCoroutine(LoadClassToChange());
+            StartCoroutine(LoadDataToChangeClass());
         }
 
-        private IEnumerator LoadClassToChange()
+        private IEnumerator LoadDataToChangeClass()
         {
             yield return null;
             _uiClassToChange.RenderClassToChange(_listCharacterClass);
+            StartCoroutine(GetWalletItemMaterial());
         }
 
+        private IEnumerator GetWalletItemMaterial()
+        {
+            _materialApi.LoadMaterialsFromWallet();
+            yield return new WaitUntil(() => _materialApi.IsFinishFetchData);
+            if (_materialApi.Data.Count <= 0) yield break;
+            //TODO: Use Material to validate change class
+        }
+        
         private IEnumerator GetWalletCharacterMaterial()
         {
             yield return _characterModel.CoGetData();
