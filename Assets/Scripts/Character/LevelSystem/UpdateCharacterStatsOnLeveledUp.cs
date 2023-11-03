01c08d7876f4953191b1bc896c5195e47a34c5e5
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-11-03 18:32:41 +0700

    refactor: decouple leveled up stats update
    move logic to saga out of leveling system

---
+++ Assets/Scripts/Character/LevelSystem/UpdateCharacterStatsOnLeveledUp.cs
@@ -0,0 +1,57 @@
+﻿using CryptoQuest.AbilitySystem.Attributes;
+using CryptoQuest.Battle.Components;
+using CryptoQuest.Gameplay.Helper;
+using CryptoQuest.Sagas;
+using UnityEngine;
+
+namespace CryptoQuest.Character.LevelSystem
+{
+    public class UpdateCharacterStatsOnLeveledUp : SagaBase<HeroLeveledUpAction>
+    {
+        [SerializeField] private AttributeWithMaxCapped[] _attributesToResetWhenLeveledUp;
+        private readonly ILevelAttributeCalculator _levelAttributeCalculator = new DefaultLevelAttributeCalculator();
+
+        protected override void HandleAction(HeroLeveledUpAction ctx) => RecalculateStats(ctx.Hero);
+
+        /// <summary>
+        /// 1. Calculate stats of last level
+        /// 2. calculate stats of current level
+        /// 3. add the addition value to base
+        /// </summary>
+        /// <param name="hero"></param>
+        private void RecalculateStats(HeroBehaviour hero)
+        {
+            if (!hero.TryGetComponent(out Battle.Components.LevelSystem levelSystem)) return;
+            var lastLevel = levelSystem.LastLevel;
+            var level = levelSystem.Level;
+            var attributeSystem = hero.AttributeSystem;
+            if (attributeSystem == null) return;
+            var attributeDefs = hero.Stats.Attributes;
+            var allowedMaxLvl = hero.Stats.MaxLevel;
+
+            foreach (var attributeDef in attributeDefs)
+            {
+                if (!attributeSystem.TryGetAttributeValue(attributeDef.Attribute, out var attributeValue)) continue;
+
+                var lastLevelBaseValue =
+                    _levelAttributeCalculator.GetValueAtLevel(lastLevel, attributeDef, allowedMaxLvl);
+                var currentLevelBaseValue =
+                    _levelAttributeCalculator.GetValueAtLevel(level, attributeDef, allowedMaxLvl);
+                var additionBaseValue = currentLevelBaseValue - lastLevelBaseValue;
+
+                attributeSystem.SetAttributeBaseValue(attributeDef.Attribute,
+                    attributeValue.BaseValue + additionBaseValue);
+            }
+
+            foreach (var attributeToReset in _attributesToResetWhenLeveledUp)
+            {
+                if (!attributeSystem.TryGetAttributeValue(attributeToReset, out var attributeValue)) continue;
+                var resetValue =
+                    attributeToReset.CalculateInitialValue(attributeValue, attributeSystem.AttributeValues);
+                attributeSystem.SetAttributeValue(attributeToReset, resetValue);
+            }
+
+            attributeSystem.UpdateAttributeValues();
+        }
+    }
+}
