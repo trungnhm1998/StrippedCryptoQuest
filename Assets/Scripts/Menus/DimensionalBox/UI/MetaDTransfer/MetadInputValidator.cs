42313e938cc98a46dbeb929a98c87f07829dc4a6
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-11-08 12:53:09 +0700

    refactor: remove unused prefab and structure

---
+++ Assets/Scripts/Menus/DimensionalBox/UI/MetaDTransfer/MetadInputValidator.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CryptoQuest.Menus.DimensionalBox.UI.MetaDTransfer
+{
+    public class MetadInputValidator : MonoBehaviour
+    {
+        [SerializeField] private InputField _inputField;
+
+        public void ValidateInputMetad()
+        {
+            if (string.IsNullOrEmpty(_inputField.text)) return;
+
+            var pattern = Regex.Match(_inputField.text, @"[^0-9]");
+            if (_inputField.text.Substring(0, 1) == pattern.ToString())
+            {
+                _inputField.text = null;
+                return;
+            }
+
+            float quantityInput = float.Parse(_inputField.text);
+            // if (_isIngameWallet && quantityInput > _ingameMetad)
+            //     _inputField.text = _ingameMetad.ToString();
+            //
+            // if (!_isIngameWallet && quantityInput > _webMetad)
+            //     _inputField.text = _webMetad.ToString();
+        }
+    }
+}
