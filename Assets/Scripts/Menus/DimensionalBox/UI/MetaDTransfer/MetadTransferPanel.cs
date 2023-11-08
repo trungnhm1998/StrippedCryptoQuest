60059fe1a7844f70231689ddb4b644b515b54bfd
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-11-08 12:53:09 +0700

    refactor: remove unused prefab and structure

---
+++ Assets/Scripts/Menus/DimensionalBox/UI/MetaDTransfer/MetadTransferPanel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CryptoQuest.Menus.DimensionalBox.UI.MetaDTransfer
+{
+    public class MetadTransferPanel : MonoBehaviour
+    {
+        [SerializeField] private Button _gameButton;
+        [SerializeField] private Button _dimensionalBoxButton;
+        [SerializeField] private InputField _inputField;
+
+        public void TransferMetadToDimensionalBox() { }
+
+        public void TransferMetadToGame() { }
+    }
+}
