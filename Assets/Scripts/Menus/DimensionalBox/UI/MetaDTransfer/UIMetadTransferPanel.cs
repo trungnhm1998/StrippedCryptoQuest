4f93deb0eae5408c2de44266bbda7041821af5b7
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-11-10 15:57:42 +0700

    feat(dbox): prevent multi transfer

--- Assets/Scripts/Menus/DimensionalBox/UI/MetaDTransfer/UIMetadTransferPanel.cs
+++ Assets/Scripts/Menus/DimensionalBox/UI/MetaDTransfer/UIMetadTransferPanel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using CryptoQuest.Core;
 using CryptoQuest.Gameplay.Inventory.Currency;
 using CryptoQuest.Gameplay.Inventory.ScriptableObjects;
 using CryptoQuest.Input;
+using CryptoQuest.UI.Actions;
 using CryptoQuest.UI.Menu;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -18,22 +20,28 @@ namespace CryptoQuest.Menus.DimensionalBox.UI.MetaDTransfer
         [field: SerializeField] public UIMetadSourceButton GameButton { get; private set; }
         [field: SerializeField] public UIMetadSourceButton DimensionalBoxButton { get; private set; }
         [field: SerializeField] public InputField TransferAmountInput { get; private set; }
+        public bool Transferring { get; set; }
 
         private void OnEnable()
         {
+            if (Transferring) ActionDispatcher.Dispatch(new ShowLoading());
             Invoke(nameof(SelectDefaultButton), 0);
         }
+
+        private void OnDisable() => ActionDispatcher.Dispatch(new ShowLoading(false));
 
         public void SelectDefaultButton() => EventSystem.current.SetSelectedGameObject(GameButton.gameObject);
 
         public void SelectGameButton()
         {
+            if (Transferring) return;
             SourceToTransfer = GameButton.GetComponentInChildren<UICurrency>().Currency;
             TransferSourceChanged?.Invoke();
         }
 
         public void SelectDimensionalBoxButton()
         {
+            if (Transferring) return;
             SourceToTransfer = DimensionalBoxButton.GetComponentInChildren<UICurrency>().Currency;
             TransferSourceChanged?.Invoke();
         }
