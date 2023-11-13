ff563b9edfdd19c4224f995b6dc72757fc9f65c4
Long18 <us.thanhlong18@gmail.com>
2023-11-13 13:55:01 +0700

    !feat: implement inn system with gold

---
+++ Assets/Scripts/Inn/InnLocalizationTable.cs
@@ -0,0 +1,12 @@
+namespace CryptoQuest.Inn
+{
+    public static class InnLocalizationTable
+    {
+        public const string INN_TABLE_REFERENCE = "Inn";
+        
+        public const string INN_WELCOME = "INN_WELCOME";
+        public const string INN_AGREE = "INN_AGREE";
+        public const string INN_CONFIRM = "INN_CONFIRM";
+        public const string INN_DO_NOT_ENOUGH_GOLD = "INN_DO_NOT_HAVE_ENOUGH_GOLD";
+    }
+}
---
+++ Assets/Scripts/Inn/ScriptableObject/ShowInnEventChannelSO.cs
@@ -0,0 +1,8 @@
+using IndiGames.Core.Events.ScriptableObjects;
+using UnityEngine;
+
+namespace Inn.ScriptableObject
+{
+    [CreateAssetMenu(menuName = "Crypto Quest/Inn/Events/Show Inn Event Channel ", fileName = "ShowInnEventChannelSO")]
+    public class ShowInnEventChannelSO : VoidEventChannelSO { }
+}
