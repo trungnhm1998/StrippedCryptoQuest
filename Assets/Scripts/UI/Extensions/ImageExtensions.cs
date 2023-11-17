bd85b96789f2686a9c4acaf00769f694c6fb1028
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-11-17 11:15:32 +0700

    feat: async image loading extension

---
+++ Assets/Scripts/UI/Extensions/ImageExtensions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
+
+namespace CryptoQuest.UI.Extensions
+{
+    public static class ImageExtensions
+    {
+        public static AsyncOperationHandle<Sprite> LoadSpriteAndSet(this Image image,
+            AssetReferenceT<Sprite> spriteAsset)
+        {
+            image.enabled = false;
+            if (spriteAsset.RuntimeKeyIsValid() == false) return default;
+            var handle = spriteAsset.LoadAssetAsync();
+            handle.Completed += handle1 =>
+            {
+                image.sprite = handle1.Result;
+                image.enabled = true;
+            };
+            return handle;
+        }
+    }
+}
