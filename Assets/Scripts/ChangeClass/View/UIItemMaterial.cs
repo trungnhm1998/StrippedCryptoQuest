03407ce12dd1cdc93a95ed03963610881523b1ab
datdiep754129 <dat.diep@indigames.net>
2023-11-20 10:53:02 +0700

    feat: import item material data

--- Assets/Scripts/ChangeClass/View/UIItemMaterial.cs
+++ Assets/Scripts/ChangeClass/View/UIItemMaterial.cs
@@ -1,25 +1,31 @@
 using CryptoQuest.ChangeClass.API;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Components;
 using UnityEngine.UI;
 
 namespace CryptoQuest.ChangeClass.View
 {
     public class UIItemMaterial : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI _displayName;
+        [SerializeField] private LocalizeStringEvent _displayName;
         [SerializeField] private TextMeshProUGUI _materialNumber;
         [SerializeField] private Image _icon;
         [SerializeField] private GameObject _defaultBackground;
-        public bool IsValid {get; private set;}
+        public bool IsValid { get; private set; }
 
         public void ConfigureCell(WalletMaterialAPI material, int quantity, int index)
         {
             var data = material.Data[index];
-            _displayName.text = data.nameEn;
             _materialNumber.text = $"{quantity}/{data.materialNum}";
             IsValid = material.Data[index].materialNum > 0;
             EnableDefaultBackground(IsValid);
+        }
+
+        public void SetLocalization(LocalizedString localized)
+        {
+            _displayName.StringReference = localized;
         }
 
         private void EnableDefaultBackground(bool isEnable)
