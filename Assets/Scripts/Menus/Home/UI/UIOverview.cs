c07c22e94d710baf553496d15476c53eb796dc3a
Anh Cao <anh.cao@indigames.net>
2023-11-21 15:20:22 +0700

    feat(home-menu): character list new UI and sub-states

---
+++ Assets/Scripts/Menus/Home/UI/UIOverview.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace CryptoQuest.Menus.Home.UI
+{
+    public class UIOverview : MonoBehaviour
+    {
+        public event UnityAction ChangeOrderEvent;
+        public event UnityAction ViewCharacterListEvent;
+
+        [SerializeField] private Button[] _overviewTabs;
+
+        public void EnableSelectActions()
+        {
+            _overviewTabs[0].Select();
+        }
+
+        public void ChangOrderButtonPressed() => ChangeOrderEvent?.Invoke();
+        public void CharacterListButtonPressed() => ViewCharacterListEvent?.Invoke();
+    }
+}
