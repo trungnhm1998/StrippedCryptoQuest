d54898b72741b38c6c17403962cbd9f278896363
Thành Long (Morningstar WiIIiam) <us.thanhlong18@gmail.com>
2023-11-22 16:22:28 +0700

    fix(ranch): remove obsolete apis (#2729)

--- Assets/Scripts/Ranch/Sagas/GetNftBeast.cs
+++ Assets/Scripts/Ranch/Sagas/GetNftBeast.cs
@@ -5,18 +5,18 @@ using CryptoQuest.API;
 using CryptoQuest.Core;
 using CryptoQuest.Networking;
 using CryptoQuest.Sagas;
+using CryptoQuest.Sagas.Objects;
 using CryptoQuest.System;
 using CryptoQuest.UI.Actions;
 using UniRx;
 using UnityEngine;
-using Obj = CryptoQuest.Sagas.Objects;
 
 namespace CryptoQuest.Ranch.Sagas
 {
     public class GetNftBeast : SagaBase<GetBeasts>
     {
-        private readonly List<Obj.Beast> _inGameBeastsCache = new();
-        private readonly List<Obj.Beast> _inBoxBeastsCache = new();
+        private readonly List<Beast> _inGameBeastsCache = new();
+        private readonly List<Beast> _inBoxBeastsCache = new();
 
         protected override void HandleAction(GetBeasts ctx)
         {
@@ -24,8 +24,7 @@ namespace CryptoQuest.Ranch.Sagas
 
             var restClient = ServiceProvider.GetService<IRestClient>();
             restClient
-                .WithParams(new Dictionary<string, string>() { { "source", $"{((int)ctx.Status).ToString()}" } })
-                .Get<Obj.BeastsResponse>(Profile.GET_BEASTS)
+                .Get<BeastsResponse>(Profile.GET_BEASTS)
                 .Subscribe(OnGetBeasts, OnError);
         }
 
@@ -36,7 +35,7 @@ namespace CryptoQuest.Ranch.Sagas
             ActionDispatcher.Dispatch(new GetNftBeastsFailed());
         }
 
-        private void OnGetBeasts(Obj.BeastsResponse response)
+        private void OnGetBeasts(BeastsResponse response)
         {
             if (response.code != (int)HttpStatusCode.OK) return;
             UpdateInGameCache(response.data.beasts);
@@ -46,28 +45,28 @@ namespace CryptoQuest.Ranch.Sagas
             ActionDispatcher.Dispatch(new GetNftBeastsSucceed());
         }
 
-        private void UpdateInGameCache(Obj.Beast[] dataBeasts)
+        private void UpdateInGameCache(Beast[] dataBeasts)
         {
             if (dataBeasts.Length == 0) return;
             _inGameBeastsCache.Clear();
 
             foreach (var beast in dataBeasts)
             {
-                if (beast.inGameStatus != (int)Obj.EBeastStatus.InGame) continue;
+                if (beast.inGameStatus != (int)EBeastStatus.InGame) continue;
                 _inGameBeastsCache.Add(beast);
             }
 
             ActionDispatcher.Dispatch(new GetInGameBeastsSucceed(_inGameBeastsCache));
         }
 
-        private void UpdateInboxCache(Obj.Beast[] dataBeasts)
+        private void UpdateInboxCache(Beast[] dataBeasts)
         {
             if (dataBeasts.Length == 0) return;
             _inBoxBeastsCache.Clear();
 
             foreach (var beast in dataBeasts)
             {
-                if (beast.inGameStatus != (int)Obj.EBeastStatus.InBox) continue;
+                if (beast.inGameStatus != (int)EBeastStatus.InBox) continue;
                 _inBoxBeastsCache.Add(beast);
             }
 
