b794495feb889a6c52ba7ad4bb7189c550a2a856
Thành Long (Morningstar WiIIiam) <us.thanhlong18@gmail.com>
2023-11-22 11:17:59 +0700

    fix: cannot interact with props (#2714)

--- Assets/Scripts/Character/MonoBehaviours/ZoneTriggerController.cs
+++ Assets/Scripts/Character/MonoBehaviours/ZoneTriggerController.cs
@@ -6,12 +6,11 @@ namespace CryptoQuest.Character.MonoBehaviours
     public class ZoneTriggerController : MonoBehaviour
     {
         [SerializeField] private UnityEvent<bool, GameObject> _enterZone = default;
-        [SerializeField] private LayerMask _layers = default;
-        public static readonly string Tag = "NPC";
+        [SerializeField] private LayerMask _layers;
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if ((1 << other.gameObject.layer & _layers) != 0 && other.tag == Tag)
+            if ((1 << other.gameObject.layer & _layers) != 0)
             {
                 _enterZone.Invoke(true, other.gameObject);
             }
@@ -19,7 +18,7 @@ namespace CryptoQuest.Character.MonoBehaviours
 
         private void OnTriggerExit2D(Collider2D other)
         {
-            if ((1 << other.gameObject.layer & _layers) != 0 && other.tag == Tag)
+            if ((1 << other.gameObject.layer & _layers) != 0)
             {
                 _enterZone.Invoke(false, other.gameObject);
             }
