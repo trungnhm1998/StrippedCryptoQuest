00b9ba00b05c45cba920a1d78af8a75abd43dacb
Thành Long (Morningstar WiIIiam) <us.thanhlong18@gmail.com>
2023-11-22 11:17:59 +0700

    fix: cannot interact with props (#2714)

--- Assets/Scripts/Gameplay/InteractableObject/Door/DoorBehaviour.cs
+++ Assets/Scripts/Gameplay/InteractableObject/Door/DoorBehaviour.cs
@@ -6,18 +6,18 @@ namespace CryptoQuest.Gameplay.InteractableObject.Door
     public class DoorBehaviour : MonoBehaviour, IInteractable
     {
         [SerializeField] private GameObject _interactableObject;
-        private bool _isOpenning = false;
+        private bool _isClosed = true;
 
         private bool _canInteract = true;
 
         public void Interact()
         {
             if (!_canInteract) return;
-            
-            _isOpenning = !_isOpenning;
-            _interactableObject.SetActive(_isOpenning);
+
+            _isClosed = !_isClosed;
+            _interactableObject.SetActive(_isClosed);
         }
-        
+
         public void SetInteract(bool canInteract)
         {
             _canInteract = canInteract;
