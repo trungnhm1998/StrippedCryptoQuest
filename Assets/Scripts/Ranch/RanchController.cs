6316656c4abb92f356fa7f3f77d4cc5bc8a483ab
Long18 <us.thanhlong18@gmail.com>
2023-11-22 14:56:03 +0700

    style: move beast sage to prefab and delete form saga scene

--- Assets/Scripts/Ranch/RanchController.cs
+++ Assets/Scripts/Ranch/RanchController.cs
@@ -25,6 +25,7 @@ namespace CryptoQuest.Ranch
 
         public void Initialize()
         {
+            RanchOpened();
             UIRanch.Initialize();
             DialogManager.ShowWelcomeDialog();
         }
