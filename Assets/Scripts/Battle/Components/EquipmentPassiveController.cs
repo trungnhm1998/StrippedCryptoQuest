1356836cb7caeabb82f0f783e30ed9cfe5bcf0ee
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-11-23 18:35:55 +0700

    fix: equipping items doesn't give passives

--- Assets/Scripts/Battle/Components/EquipmentPassiveController.cs
+++ Assets/Scripts/Battle/Components/EquipmentPassiveController.cs
@@ -1,56 +1,51 @@
 ﻿using System.Collections.Generic;
 using CryptoQuest.AbilitySystem.Abilities;
 using CryptoQuest.Item.Equipment;
+using IndiGames.GameplayAbilitySystem.AbilitySystem.Components;
+using UnityEngine;
 
 namespace CryptoQuest.Battle.Components
 {
-    public class EquipmentPassiveController : CharacterComponentBase
+    public class EquipmentPassiveController : MonoBehaviour
     {
         private EquipmentsController _equipmentsController;
 
         private readonly Dictionary<EquipmentInfo, PassiveAbilitySpec[]> _passives = new();
+        private AbilitySystemBehaviour _abilitySystem;
 
-        protected override void Awake()
+        protected void Awake()
         {
-            base.Awake();
             _equipmentsController = GetComponent<EquipmentsController>();
+            _abilitySystem = GetComponent<AbilitySystemBehaviour>();
 
-            // _equipmentsController.Equipped += GrantPassive;
-            // _equipmentsController.Removed += RemovePassive;
+            _equipmentsController.Equipped += GrantPassive;
+            _equipmentsController.Removed += RemovePassive;
         }
 
         private void OnDestroy()
         {
-            // _equipmentsController.Equipped -= GrantPassive;
-            // _equipmentsController.Removed -= RemovePassive;
+            _equipmentsController.Equipped -= GrantPassive;
+            _equipmentsController.Removed -= RemovePassive;
         }
-
-        public override void Init() { }
 
         private void GrantPassive(EquipmentInfo equipment)
         {
-            if(equipment.Passives.Length > 0)
+            var passiveSpecs = new PassiveAbilitySpec[equipment.Passives.Length];
+            for (var index = 0; index < equipment.Passives.Length; index++)
             {
-                var passiveSpecs = new PassiveAbilitySpec[equipment.Passives.Length];
-                for (var index = 0; index < equipment.Passives.Length; index++)
-                {
-                    var passive = equipment.Passives[index];
-                    if (passive == null) continue;
-                    var spec = Character.AbilitySystem.GiveAbility<PassiveAbilitySpec>(passive);
-                    passiveSpecs[index] = spec;
-                }
-                _passives.Add(equipment, passiveSpecs);
+                var passive = equipment.Passives[index];
+                if (passive == null) continue;
+                var spec = _abilitySystem.GiveAbility<PassiveAbilitySpec>(passive);
+                passiveSpecs[index] = spec;
             }
+
+            if (passiveSpecs.Length > 0) _passives.Add(equipment, passiveSpecs);
         }
 
         private void RemovePassive(EquipmentInfo equipment)
         {
             if (_passives.TryGetValue(equipment, out var passives) == false) return;
-            foreach (var passive in passives)
-            {
-                Character.AbilitySystem.RemoveAbility(passive);
-            }
-
+            foreach (var passive in passives) _abilitySystem.RemoveAbility(passive);
             _passives.Remove(equipment);
         }
     }
