2e641b7c7283b7696c0fb21be37322cfe09da1b6
minhnguyen0699 <minh.nguyen@indigames.net>
2023-11-24 16:08:39 +0700

    fix: refactor check and apply cost flow logger

---
+++ Assets/Scripts/Battle/UI/Logs/NotEnoughMpMenuLogger.cs
@@ -0,0 +1,33 @@
+using CryptoQuest.Core;
+using TinyMessenger;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Localization;
+
+namespace CryptoQuest.Battle.UI.Logs
+{
+    public class NotEnoughMpMenuLog : ActionBase { }
+
+    public class NotEnoughMpMenuLogger : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent<LocalizedString> _presentLoggerEvent;
+        [SerializeField] private LocalizedString _localizedLog;
+        private TinyMessageSubscriptionToken _token;
+
+        private void OnEnable()
+        {
+            _token = ActionDispatcher.Bind<NotEnoughMpMenuLog>(HandleLog);
+        }
+
+        private void OnDisable()
+        {
+            ActionDispatcher.Unbind(_token);
+        }
+
+        private void HandleLog(NotEnoughMpMenuLog _)
+        {
+            var msg = new LocalizedString(_localizedLog.TableReference, _localizedLog.TableEntryReference);
+            _presentLoggerEvent.Invoke(msg);
+        }
+    }
+}
