5fcf8ff09f1aef15be6ecc5ca1ced6dd4a93726f
Nagahara <107096882+HironoriNagahara@users.noreply.github.com>
2023-11-24 15:51:09 +0900

    feat: setup chap 3 cutscenes (#2730)

---
+++ Assets/Arts/Maps/Chapter6/47_demon_Castle_shrine/47_demon_Castle_shrine/47_tmx/000_hero_muddy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="000_hero_muddy" tilewidth="32" tileheight="32" tilecount="3" columns="1">
+ <image source="../../../../../../../../../../../Downloads/000_hero_muddy.png" width="51" height="108"/>
+</tileset>
