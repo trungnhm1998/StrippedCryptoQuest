20b1f314ac73c575adfda18f0f6c6917e130fa5e
datdiep754129 <dat.diep@indigames.net>
2023-11-27 16:47:03 +0700

    feat(cheat): add debug beast

--- Assets/API/Cheats.cs
+++ Assets/API/Cheats.cs
@@ -5,6 +5,7 @@ namespace CryptoQuest.API
     {
         public const string CREATE_DEBUG_NFT_EQUIPMENT = "crypto/debug/equipments/add-nft";
         public const string CREATE_DEBUG_CHARACTER = "crypto/debug/characters/add-nft";
+        public const string CREATE_DEBUG_BEAST = "crypto/debug/beasts/add-nft";
         public const string CREATE_CLASS_MATERIAL = "crypto/materials";
     }
 }
