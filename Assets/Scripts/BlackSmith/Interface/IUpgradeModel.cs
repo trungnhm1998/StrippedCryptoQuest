162c7daa50587f148b46285419a568675cfd3d32
Viet <viet.hoang@indigames.net>
2023-11-27 17:42:53 +0900

    fix: error when enter upgrade menu cause by equipment data

--- Assets/Scripts/BlackSmith/Interface/IUpgradeModel.cs
+++ Assets/Scripts/BlackSmith/Interface/IUpgradeModel.cs
@@ -7,6 +7,6 @@ namespace CryptoQuest.BlackSmith.Interface
     public interface IUpgradeModel
     {
         public List<IUpgradeEquipment> ListEquipment { get; }
-        public void CoGetData(InventorySO inventory);
+        public IEnumerator CoGetData(InventorySO inventory);
     }
 }
--- Assets/Scripts/BlackSmith/Upgrade/UpgradeModel.cs
+++ Assets/Scripts/BlackSmith/Upgrade/UpgradeModel.cs
@@ -1,24 +1,59 @@
+using System.Collections;
 using System.Collections.Generic;
 using CryptoQuest.BlackSmith.Interface;
 using CryptoQuest.Gameplay.Inventory.ScriptableObjects;
+using CryptoQuest.Item.Equipment;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Localization;
 
 namespace CryptoQuest.BlackSmith.Upgrade
 {
+    public class UpgradeEquipmentData : IUpgradeEquipment
+    {
+        public UpgradeEquipmentData(EquipmentInfo equipment)
+        {
+            Equipment = equipment;
+        }
+
+        public EquipmentInfo Equipment { get; private set; }
+
+        public LocalizedString DisplayName => Equipment.DisplayName;
+
+        public Sprite Icon => Equipment.Config.EquipmentType.Icon;
+
+        public Sprite Rarity => Equipment.Rarity.Icon;
+
+        public AssetReferenceT<Sprite> Illustration => Equipment.Config.Image;
+
+        // TODO: Get cost from database #2417
+        public float Cost { get; set; }
+
+        public int Level => Equipment.Level;
+    }
+
     public class UpgradeModel : MonoBehaviour, IUpgradeModel
     {
-        private List<IUpgradeEquipment> _upgradeData;
-        public List<IUpgradeEquipment> ListEquipment => _upgradeData;
-        public void CoGetData(InventorySO inventory)
+        [SerializeField] private EquipmentPrefabDatabase _equipmentPrefabDatabase;
+        private List<IUpgradeEquipment> _equipmentData;
+        public List<IUpgradeEquipment> ListEquipment => _equipmentData;
+
+        public IEnumerator CoGetData(InventorySO inventory)
         {
-            // _upgradeData = new();
-            // var listEquipment = inventory.Equipments;
-            // foreach (var equipment in listEquipment)
-            // {
-            //     IUpgradeEquipment equipmentData = new MockUpgradeEquipment(equipment);
-            //     if (equipment.Level < equipment.Data.MaxLevel)
-            //         _upgradeData.Add(equipmentData);
-            // }
+            _equipmentData = new();
+            var listEquipment = inventory.Equipments;
+            foreach (var equipment in listEquipment)
+            {
+                if (equipment.Config == null)
+                {
+                    yield return _equipmentPrefabDatabase.LoadDataById(equipment.Data.PrefabId);
+                    equipment.Config = _equipmentPrefabDatabase.GetDataById(equipment.Data.PrefabId);
+                }
+
+                IUpgradeEquipment equipmentData = new UpgradeEquipmentData(equipment);
+                if (equipment.Level < equipment.Data.MaxLevel)
+                    _equipmentData.Add(equipmentData);
+            }
         }
     }
 }
