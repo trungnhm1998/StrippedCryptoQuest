cb5e883a948a09865d8b616ea3b286a8ce46e6d3
Anh Cao <anh.cao@indigames.net>
2023-11-28 17:40:38 +0700

    feat(home-menu): fetch and render character list

---
+++ Assets/Scripts/Character/Sagas/Actions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CryptoQuest.Core;
+using Obj = CryptoQuest.Sagas.Objects;
+
+namespace CryptoQuest.Character.Sagas
+{
+    public class GetInGameHeroes : ActionBase
+    {
+        public Obj.ECharacterStatus Status { get; set; } = Obj.ECharacterStatus.All;
+    }
+
+    public class FetchInGameHeroesSucceeded : ActionBase
+    {
+        public List<Obj.Character> InGameHeroes { get; }
+
+        public FetchInGameHeroesSucceeded(List<Obj.Character> inGameHeroes)
+        {
+            InGameHeroes = inGameHeroes;
+        }
+    }
+
+    public class FetchInGameHeroesFailed : ActionBase { }
+}
--- Assets/Scripts/Menus/Home/States/CharacterListState.cs
+++ Assets/Scripts/Menus/Home/States/CharacterListState.cs
@@ -1,23 +1,37 @@
-﻿using FSM;
+﻿using CryptoQuest.Character.Sagas;
+using CryptoQuest.Core;
+using FSM;
 using CryptoQuest.Menus.Home.UI;
+using TinyMessenger;
 
 namespace CryptoQuest.Menus.Home.States
 {
     public class CharacterListState : StateBase
     {
         private UIHomeMenu _homePanel;
+        private TinyMessageSubscriptionToken _fetchHeroesSucceeded;
+
         public CharacterListState(UIHomeMenu panel) : base(false) => _homePanel = panel;
 
         public override void OnEnter()
         {
+            _fetchHeroesSucceeded = ActionDispatcher.Bind<FetchInGameHeroesSucceeded>(PassDataToUi);
             _homePanel.Input.MenuCancelEvent += HandleCancel;
             _homePanel.UICharacterList.gameObject.SetActive(true);
+            
+            ActionDispatcher.Dispatch(new GetInGameHeroes());
         }
 
         public override void OnExit()
         {
+            ActionDispatcher.Unbind(_fetchHeroesSucceeded);
             _homePanel.Input.MenuCancelEvent -= HandleCancel;
             _homePanel.UICharacterList.gameObject.SetActive(false);
+        }
+
+        private void PassDataToUi(FetchInGameHeroesSucceeded ctx)
+        {
+            _homePanel.UICharacterList.SetData(ctx.InGameHeroes);
         }
 
         private void HandleCancel()
