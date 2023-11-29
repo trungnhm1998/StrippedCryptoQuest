82538f3930b13722ccadb803acf14c7622b4783a
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-11-29 21:23:26 +0700

    refactor: decouple previewer from inventory panel

---
+++ Assets/Scripts/UI/Menu/PreviewerTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace CryptoQuest.UI.Menu
+{
+    public class PreviewerTrigger : MonoBehaviour, ISelectHandler, IDeselectHandler
+    {
+        public void OnSelect(BaseEventData eventData) { }
+
+        public void OnDeselect(BaseEventData eventData) { }
+    }
+}
