2b1cd6a0bbd54346c87c48cf16486ee84252f367
Viet <viet.hoang@indigames.net>
2023-12-04 16:26:10 +0900

    feat: upgrade equipment API

--- Assets/Scripts/BlackSmith/Upgrade/Sagas/RequestUpgradeSaga.cs
+++ Assets/Scripts/BlackSmith/Upgrade/Sagas/RequestUpgradeSaga.cs
@@ -1,18 +1,86 @@
+using System;
+using System.Net;
 using CryptoQuest.BlackSmith.Upgrade.Actions;
 using CryptoQuest.Networking;
+using CryptoQuest.Sagas.Objects;
 using CryptoQuest.UI.Actions;
 using IndiGames.Core.Common;
 using IndiGames.Core.Events;
+using Newtonsoft.Json;
+using UnityEngine;
+using UniRx;
 
 namespace CryptoQuest.BlackSmith.Upgrade.Sagas
 {
     public class RequestUpgradeSaga : SagaBase<RequestUpgrade>
     {
+        [Serializable]
+        public struct RequestUpgradeBody
+        {
+            [JsonProperty("beforeLv")]
+            public int BeforeLevel;
+
+            [JsonProperty("equipment")]
+            public EquipmentRequestData Equipment;
+        }
+
+        [Serializable]
+        public struct EquipmentRequestData
+        {
+            [JsonProperty("id")]
+            public uint Id;
+
+            [JsonProperty("equipmentId")]
+            public string EquipmentId;
+
+            [JsonProperty("lv")]
+            public int Lv;
+        }
+
+        public const string UPGRADE_API = "crypto/equipments/upgrade"; 
+
+        private UpgradeResponse _response;
+        private IRestClient _restClient;
+
         protected override void HandleAction(RequestUpgrade ctx)
         {
             ActionDispatcher.Dispatch(new ShowLoading());
-            var restClient = ServiceProvider.GetService<IRestClient>();
-            // TODO: Subscribe and dispact UpgradeResponsed action here #2174
+            _restClient ??= ServiceProvider.GetService<IRestClient>();
+            var equipment = ctx.EquipmentToUpgrade;
+
+            var requestBody = new RequestUpgradeBody()
+            {
+                BeforeLevel = equipment.Level,
+                Equipment = new EquipmentRequestData()
+                {
+                    Id = equipment.Id,
+                    Lv = ctx.UpgradeLevel,
+                    EquipmentId = equipment.Data.ID
+                }
+            };
+            
+            _restClient.WithBody(requestBody)
+                .Post<UpgradeResponse>(UPGRADE_API)
+                .Subscribe(GotResponse, DispatchUpgradeFailed, DispatchUpgradeFinished);
+        }
+
+        private void GotResponse(UpgradeResponse response)
+        {
+            if (response.code != (int)HttpStatusCode.OK) return;
+            _response = response;
+        }
+        
+        private void DispatchUpgradeFailed(Exception obj)
+        {
+            Debug.LogWarning($"Upgrade failed!\n{obj}");
+            ActionDispatcher.Dispatch(new ShowLoading(false));
+            ActionDispatcher.Dispatch(new UpgradeFailed());
+        }
+
+        private void DispatchUpgradeFinished()
+        {
+            ActionDispatcher.Dispatch(new ShowLoading(false));
+            ActionDispatcher.Dispatch(new UpgradeResponsed(_response));
         }
     }
 }
