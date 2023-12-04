49ddc940592e6040cf56f8ac7421edd5d9d33691
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-12-04 09:23:02 +0700

    feat(merchant): generic merchant system base

--- Assets/Scripts/BlackSmith/BlackSmithSystem.cs
+++ Assets/Scripts/BlackSmith/BlackSmithSystem.cs
@@ -2,50 +2,29 @@ using CryptoQuest.BlackSmith.Evolve.UI;
 using CryptoQuest.BlackSmith.States.Overview;
 using CryptoQuest.BlackSmith.Upgrade;
 using CryptoQuest.Input;
-using IndiGames.Core.Events.ScriptableObjects;
+using CryptoQuest.Merchant;
 using UnityEngine;
 
 namespace CryptoQuest.BlackSmith
 {
-    public class BlackSmithSystem : MonoBehaviour
+    public class BlackSmithSystem : MerchantSystemBase
     {
-        [SerializeField] private GameObject _canvas;
         [field: SerializeField] public MerchantsInputManager Input { get; private set; }
         [field: SerializeField] public UIBlackSmithOverview OverviewUI { get; private set; }
         [field: SerializeField] public BlackSmithDialogsPresenter DialogPresenter { get; private set; }
         [field: SerializeField] public UpgradePresenter UpgradePresenter { get; private set; }
         [field: SerializeField] public EvolveSystem EvolveSystem { get; private set; }
 
-        [Header("Listening to")]
-        [field: SerializeField] public VoidEventChannelSO OpenSystemEvent;
-        [field: SerializeField] public VoidEventChannelSO CloseSystemEvent;
-
         private BlackSmithStateMachine _stateMachine;
 
-        private void Awake() => _stateMachine = new(this);
-
-        private void OnEnable()
+        protected override void OnInit()
         {
-            OpenSystemEvent.EventRaised += InitSystem;
-            CloseSystemEvent.EventRaised += CloseSystem;
-        }
-
-        private void OnDisable()
-        {
-            OpenSystemEvent.EventRaised -= InitSystem;
-            CloseSystemEvent.EventRaised += CloseSystem;
-            CloseSystem();
-        }
-
-        private void InitSystem()
-        {
-            _canvas.SetActive(true);
+            _stateMachine ??= new(this);
             _stateMachine.Init();
         }
 
-        private void CloseSystem()
+        protected override void OnExit()
         {
-            _canvas.SetActive(false);
             _stateMachine.OnExit();
         }
     }
