7ffe34f27adb678e43156dd6b29456283ed2c2ba
lananhcao <anh.cao@indigames.net>
2023-12-06 16:17:25 +0700

    feat(magic-stone): select first button nav bar

---
+++ Assets/Scripts/Menus/Status/UI/MagicStone/UIFilterStoneElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CryptoQuest.Menus.Status.UI.MagicStone
+{
+    public class UIFilterStoneElement : MonoBehaviour
+    {
+        [SerializeField] private Button[] _navButtons;
+
+        private void OnEnable()
+        {
+            _navButtons[0].Select();
+        }
+    }
+}
