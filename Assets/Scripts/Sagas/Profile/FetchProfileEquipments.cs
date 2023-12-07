50f0de88b1518b50e0082d90c33d3614e51601ac
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-12-07 19:50:58 +0700

    refactor: equipments response converter

--- Assets/Scripts/Sagas/Profile/FetchProfileEquipments.cs
+++ Assets/Scripts/Sagas/Profile/FetchProfileEquipments.cs
@@ -1,68 +1,26 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Reflection;
-using CryptoQuest.AbilitySystem.Abilities;
 using CryptoQuest.Actions;
-using CryptoQuest.Events;
-using CryptoQuest.Gameplay.Inventory.ScriptableObjects;
-using CryptoQuest.Item;
-using CryptoQuest.Item.Equipment;
+using CryptoQuest.Gameplay.Inventory;
 using CryptoQuest.Networking;
 using CryptoQuest.Sagas.Objects;
 using CryptoQuest.UI.Actions;
 using IndiGames.Core.Common;
 using IndiGames.Core.Events;
-using IndiGames.GameplayAbilitySystem.AttributeSystem.ScriptableObjects;
 using UniRx;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 using APIProfile = CryptoQuest.API.Profile;
 
 namespace CryptoQuest.Sagas.Profile
 {
     public class FetchProfileEquipments : SagaBase<FetchProfileEquipmentsAction>
     {
-        [SerializeField] private EquipmentPrefabDatabase _prefabDatabase;
-        [SerializeField] private GetEquipmentsEvent _inGameEquipmentsUpdate;
-        [SerializeField] private PassiveAbilityDatabase _passiveAbilityDatabase;
-        [SerializeField] private AssetReferenceT<InventorySO> _inventoryAsset;
-        private InventorySO _inventory;
-
-        /// <summary>
-        /// Map the response attribute name to the attribute scriptable object
-        /// </summary>
-        [SerializeField] private List<ResponseAttributeMap> _attributeMap = new();
-
-        [SerializeField] private List<RaritySO> _rarities = new();
-
-        private Dictionary<string, AttributeScriptableObject> _lookupAttribute = new();
-        private FieldInfo[] _fields;
-
-        private void Awake()
-        {
-            _lookupAttribute = _attributeMap.ToDictionary(map => map.Name, map => map.Attribute);
-        }
-
-        protected override void OnEnable()
-        {
-            base.OnEnable();
-            _inGameEquipmentsUpdate.EventRaised += RefreshInventoryWithOnline;
-        }
-
-        protected override void OnDisable()
-        {
-            base.OnDisable();
-            _inGameEquipmentsUpdate.EventRaised -= RefreshInventoryWithOnline;
-        }
-
-        private void RefreshInventoryWithOnline(List<EquipmentResponse> nftEquipmentsResponse)
-            => OnInventoryFilled(nftEquipmentsResponse.ToArray());
+        [SerializeField] private InventoryControllerSO _inventoryController;
 
         protected override void HandleAction(FetchProfileEquipmentsAction _)
         {
+            ActionDispatcher.Dispatch(new ShowLoading());
             var restClient = ServiceProvider.GetService<IRestClient>();
             restClient
                 .WithParams(new Dictionary<string, string>
@@ -73,7 +31,6 @@ namespace CryptoQuest.Sagas.Profile
 
         private void ProcessResponseEquipments(EquipmentsResponse response)
         {
-            ActionDispatcher.Dispatch(new ShowLoading(false));
             if (response.code != (int)HttpStatusCode.OK) return;
             var responseEquipments = response.data.equipments;
             if (responseEquipments.Length == 0) return;
@@ -81,90 +38,16 @@ namespace CryptoQuest.Sagas.Profile
         }
 
         private void OnInventoryFilled(EquipmentResponse[] responseEquipments)
-            => StartCoroutine(CoLoadAndUpdateInventory(responseEquipments));
-
-        private IEnumerator CoLoadAndUpdateInventory(EquipmentResponse[] responseEquipments)
         {
-            if (_inventory == null)
-            {
-                var handle = _inventoryAsset.LoadAssetAsync();
-                yield return handle;
-                _inventory = handle.Result;
-            }
-
-            var equipments = new List<NftEquipment>();
+            var converter = ServiceProvider.GetService<IEquipmentResponseConverter>();
+            _inventoryController.Inventory.NftEquipments.Clear();
+            _inventoryController.Inventory.Equipments.Clear();
             foreach (var equipmentResponse in responseEquipments)
             {
-                yield return _prefabDatabase.LoadDataByIdAsync(equipmentResponse.equipmentIdForeign);
-                var equipment = CreateNftEquipment(equipmentResponse,
-                    _prefabDatabase.GetDataById(equipmentResponse.equipmentIdForeign));
-                equipments.Add(equipment);
+                var equipment = converter.Convert(equipmentResponse);
+                equipment.AddToInventory(_inventoryController);
             }
-
-            _inventory.NftEquipments.Clear();
-            _inventory.NftEquipments.AddRange(equipments);
             ActionDispatcher.Dispatch(new InventoryFilled());
-        }
-
-        private NftEquipment CreateNftEquipment(EquipmentResponse equipmentResponse, EquipmentPrefab prefab)
-        {
-            var nftEquipment = new NftEquipment
-            {
-                Id = equipmentResponse.id,
-                TokenId = equipmentResponse.equipmentTokenId,
-                Level = equipmentResponse.lv
-            };
-            FillEquipmentData(equipmentResponse, ref nftEquipment, prefab);
-            return nftEquipment;
-        }
-
-        private void FillEquipmentData(EquipmentResponse response, ref NftEquipment nftEquipment,
-            EquipmentPrefab prefab)
-        {
-            nftEquipment.Data = new EquipmentData()
-            {
-                ID = response.equipmentId,
-                Prefab = prefab,
-                Rarity = _rarities.FirstOrDefault(rarity => rarity.ID == response.rarityId),
-                Stars = response.star,
-                RequiredCharacterLevel = response.restrictedLv,
-                MinLevel = response.minLv,
-                MaxLevel = response.maxLv,
-                ValuePerLvl = response.valuePerLv,
-                Stats = GetStats(response)
-            };
-            StartCoroutine(FillEquipmentSkills(response, nftEquipment));
-        }
-
-        private IEnumerator FillEquipmentSkills(EquipmentResponse response, NftEquipment nftEquipment)
-        {
-            var skills = new List<int>(response.conditionSkills);
-            skills.AddRange(response.passiveSkills);
-
-            var passiveList = new List<PassiveAbility>();
-            foreach (var skillId in skills)
-            {
-                yield return _passiveAbilityDatabase.LoadDataById(skillId);
-                passiveList.Add(_passiveAbilityDatabase.GetDataById(skillId));
-            }
-
-            nftEquipment.Data.Passives = passiveList.ToArray();
-        }
-
-        private AttributeWithValue[] GetStats(EquipmentResponse equipmentResponse)
-        {
-            var stats = new List<AttributeWithValue>();
-            // using reflection here, might optimize if this hits performance
-            _fields ??= typeof(EquipmentResponse).GetFields();
-            foreach (var fieldInfo in _fields)
-            {
-                if (_lookupAttribute.TryGetValue(fieldInfo.Name, out var attributeSO) == false) continue;
-                var value = (float)fieldInfo.GetValue(equipmentResponse);
-                if (value <= 0) continue;
-                stats.Add(new AttributeWithValue(attributeSO, value));
-            }
-
-            return stats.ToArray();
         }
 
         private void OnError(Exception error)
@@ -172,6 +55,6 @@ namespace CryptoQuest.Sagas.Profile
             Debug.Log("FetchProfileEquipments::OnError " + error);
         }
 
-        private void OnCompleted() { }
+        private void OnCompleted() => ActionDispatcher.Dispatch(new ShowLoading(false));
     }
 }
---
+++ Assets/Tests/Runtime/Profile/EquipmentResponseConverterTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using CryptoQuest.Sagas.Objects;
+using CryptoQuest.Sagas.Profile;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace CryptoQuest.Tests.Runtime.Profile
+{
+    [TestFixture, Category("Smokes")]
+    public class EquipmentResponseConverterTests
+    {
+        private IEquipmentResponseConverter _responseConverter;
+
+        [OneTimeSetUp]
+        public void OnetimeSetup()
+        {
+            var converter = AssetDatabase.LoadAssetAtPath<EquipmentResponseConverter>(
+                "Assets/Prefabs/Providers/EquipmentProvider.prefab");
+            _responseConverter = Object.Instantiate(converter);
+        }
+
+        [UnityTest]
+        public IEnumerator Converter_ShouldLoadDataCorrect()
+        {
+            var json =
+                "{\"id\":62,\"equipmentTokenId\":\"519\",\"userId\":\"zeYhVkERAAgODkQp75Y728Tfd6V2\",\"walletAddress\":\"0xe95620f7fff06812ce318a6d22d30290fb556b79\",\"lv\":1,\"equipmentId\":\"201015000421\",\"equipTypeId\":101,\"attack\":552,\"addAttack\":3,\"MATK\":0,\"addMATK\":0,\"defence\":0,\"addDefence\":0,\"criticalRate\":0,\"evasionRate\":0,\"attachUnitTokenId\":\"\",\"inGameStatus\":2,\"mintStatus\":2,\"transferring\":0,\"createdAt\":\"2023-11-15T10:41:15.000Z\",\"updatedAt\":\"2023-11-15T10:41:15.000Z\",\"equipmentIdForeign\":\"10290\",\"localizeKey\":\"DAGGER_ULTIMATEBREAKER\",\"equipNameJp\":\"アルティメットブレイカー\",\"equipTypeNameJp\":\"短剣\",\"seriesNameJp\":\"\",\"equipName\":\"UltimateBreaker\",\"equipTypeName\":\"Dagger\",\"rarityName\":\"legend\",\"categoryId\":\"0\",\"seriesName\":\"\",\"equipPartId\":\"1\",\"rarityId\":50,\"seriesId\":\"\",\"groupId\":\"10150004\",\"star\":2,\"evoLv\":0,\"restrictedLv\":0,\"price\":0,\"sellingPrice\":0,\"minLv\":10,\"maxLv\":20,\"minAttack\":549,\"minMATK\":0,\"minDefence\":0,\"minEvasionRate\":0,\"minCriticalRate\":0,\"maxAttack\":699,\"maxMATK\":0,\"maxDefence\":0,\"maxEvasionRate\":0,\"maxCriticalRate\":0,\"miningPower\":2413,\"consumeFuel\":2413,\"imageFileName\":\"NFT-WA_91-01\",\"passiveSkillId1\":4004,\"passiveSkillId2\":0,\"conditionSkillId\":0,\"nft\":1,\"slot\":3,\"imageURL\":\"https://bafybeigi2gcr3fpy22tv3u32ewu5goe23u3544nteadyjtts65zvmj24ui.ipfs.nftstorage.link/NFT-WA_91-01.png\",\"randomNumberBonus\":9,\"valuePerLv\":15,\"attachStones\":{},\"passiveSkills\":[4004],\"conditionSkills\":[]}";
+            var equipmentResponse = JsonUtility.FromJson<EquipmentResponse>(json);
+            var equipment = _responseConverter.Convert(equipmentResponse);
+
+            Assert.AreEqual(equipmentResponse.id, equipment.Id);
+            Assert.AreEqual(equipmentResponse.equipmentId, equipment.Data.ID);
+            Assert.AreEqual(equipmentResponse.star, equipment.Data.Stars);
+            Assert.AreEqual(equipmentResponse.restrictedLv, equipment.Data.RequiredCharacterLevel);
+            Assert.AreEqual(equipmentResponse.rarityId, equipment.Data.Rarity.ID);
+
+            var frameCount = 0;
+            while (equipment.Data.Prefab == null || equipment.Passives.Length == 0)
+            {
+                frameCount++;
+                if (frameCount >= 100)
+                {
+                    Assert.Fail("Cannot load prefab and skill");
+                    break;
+                    yield break;
+                }
+
+                yield return null;
+            }
+
+            Assert.AreEqual(equipmentResponse.equipmentIdForeign, equipment.Data.Prefab.ID);
+            Assert.AreEqual(equipmentResponse.passiveSkills[0], equipment.Passives[0].Id);
+        }
+    }
+}
