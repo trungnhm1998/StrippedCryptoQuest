a1b084660d93d75381ce77ea5fb241689bf188bc
Viet <viet.hoang@indigames.net>
2023-12-11 19:09:00 +0900

    feat: generic error popup

---
+++ Assets/Scripts/UI/Popups/BasePopupController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.Pool;
+
+namespace CryptoQuest.UI.Popups
+{
+    public abstract class BasePopupController<T> : MonoBehaviour where T : UIPopup
+    {
+        [SerializeField] protected PopupInputManager _inputManager;
+        [SerializeField] private Transform _popupParent;
+        [SerializeField] private AssetReference _prefab;
+        [SerializeField] private float _autoReleaseTimeout = 120f;
+
+        private AsyncOperationHandle<GameObject> _handler;
+        // I used pool because there might be many popup show at the same time
+        private ObjectPool<T> _popupPool;
+        protected readonly List<T> _popups = new();
+
+        public bool IsPopupsEmpty => _popups.Count <= 0;
+
+        private void Awake()
+        {
+            if (_popupParent == null) _popupParent = transform;
+            _popupPool = new ObjectPool<T>(OnCreate, OnGet, OnRelease, OnDestroyPopup);
+        }
+
+        protected void ShowPopup(Action<T> popupCallback)
+        {
+            StartCoroutine(CoShowPopup(popupCallback));
+        }
+
+        private IEnumerator CoShowPopup(Action<T> popupCallback)
+        {
+            if (_popups.Count > 0 || (_handler.IsDone && _handler.IsValid()))
+            {
+                popupCallback?.Invoke(_popupPool.Get());
+                yield break;
+            }
+
+            _handler = _prefab.LoadAssetAsync<GameObject>();
+            yield return _handler;
+            if (!_handler.IsDone) yield break;
+            popupCallback?.Invoke(_popupPool.Get());
+        }
+
+        protected void Hide(T popup)
+        {
+            _popupPool.Release(popup);
+
+            if (IsPopupsEmpty) _inputManager.DisableInput();
+        }
+
+        private void ReleaseAssets()
+        {
+            // If popup empty, release popup asset after _autoReleaseTimeout seconds
+            // If there new popup added the release will be cancel 
+            if (_popups.Count > 0) return;
+
+            // This should call OnDestroyPopup and destroy all pooled popup
+            _popupPool.Dispose();
+
+            if (_handler.IsValid()) 
+            {
+                Addressables.Release(_handler);
+                _prefab.ReleaseAsset();
+            }
+        }
+
+        private T OnCreate()
+        {
+            var gameObject = Instantiate(_handler.Result, _popupParent);
+            var popup = gameObject.GetComponent<T>();
+            return popup;
+        }
+
+        private void OnGet(T popup)
+        {
+            popup.gameObject.SetActive(true);
+            _inputManager.EnableInput();
+
+            if (_popups.Contains(popup)) return;
+            _popups.Add(popup);
+        }
+
+        private void OnRelease(T popup)
+        {
+            popup.gameObject.SetActive(false);
+
+            _popups.Remove(popup);
+
+            Invoke(nameof(ReleaseAssets), _autoReleaseTimeout);
+        }
+
+        private void OnDestroyPopup(T popup)
+        {
+            Destroy(popup.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Popups/PopupInputManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using CryptoQuest.Input;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
+
+namespace CryptoQuest.UI.Popups
+{
+    // Cache last action map and disable all input and after close popup enable cached action map
+    public class PopupInputManager : MonoBehaviour, InputActions.IPopupActions
+    {
+        public static string POPUP_ACTION_MAP_NAME = "Popup";
+        public event Action ClosePopupEvent;
+
+        // Disable this to prevent user can still navigate or confirm while popup is showing
+        [SerializeField] private InputSystemUIInputModule _inputSystemModule;
+        [SerializeField] private InputMediatorSO _inputSO;
+        private string _previouslyEnabledInputMap = "";
+
+        private void EnableLastEnabledActionMap()
+        {
+            if (string.IsNullOrEmpty(_previouslyEnabledInputMap) == false)
+                _inputSO.EnableInputMap(_previouslyEnabledInputMap);
+        }
+
+        private void CacheLastEnabledActionMap()
+        {
+            var assetActionMaps = _inputSO.InputActions.asset.actionMaps;
+            foreach (var actionMap in assetActionMaps)
+            {
+                if (actionMap.enabled && actionMap.name != POPUP_ACTION_MAP_NAME)
+                {
+                    _previouslyEnabledInputMap = actionMap.name;
+                    break;
+                }
+            }
+        }
+
+        public void EnableInput()
+        {
+            _inputSO.InputActions.Popup.SetCallbacks(this);
+            CacheLastEnabledActionMap();
+            _inputSO.DisableAllInput();
+            _inputSO.InputActions.Popup.Enable();
+            _inputSystemModule.enabled = false;
+        }
+        
+        public void DisableInput()
+        {
+            _inputSO.InputActions.Popup.Disable();
+            _inputSO.InputActions.Popup.RemoveCallbacks(this);
+            _inputSystemModule.enabled = true;
+            EnableLastEnabledActionMap();
+        }
+
+        public void OnCancel(InputAction.CallbackContext context)
+        {
+            if (context.performed) ClosePopupEvent?.Invoke();
+        }
+
+        public void OnConfirm(InputAction.CallbackContext context)
+        {
+            if (context.performed) ClosePopupEvent?.Invoke();
+        }
+    }
+}
