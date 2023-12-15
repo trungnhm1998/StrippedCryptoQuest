12d5054e79663e5630aeba7a715ab2050812077d
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-12-15 09:00:30 +0700

    refactor(shop)!: remove old system

--- Assets/Scripts/Shop/UI/ScriptableObjects/ShopStateSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace CryptoQuest.Shop.UI.ScriptableObjects
-{
-    [CreateAssetMenu(fileName = "ShopState", menuName = "Crypto Quest/Shop/Shop State")]
-    public class ShopStateSO : ScriptableObject { }
-}
