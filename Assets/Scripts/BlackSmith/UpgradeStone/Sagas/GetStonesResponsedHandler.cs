059aabdb0f7d7c218a9a514fc07a1c4bd2531908
Viet <viet.hoang@indigames.net>
2023-12-19 12:30:51 +0900

    ﻿fix: game freeze when user dont have stone enter upgrade stone (#3157)

--- Assets/Scripts/BlackSmith/UpgradeStone/Sagas/GetStonesResponsedHandler.cs
+++ Assets/Scripts/BlackSmith/UpgradeStone/Sagas/GetStonesResponsedHandler.cs
@@ -3,19 +3,20 @@ using CryptoQuest.Item.MagicStone;
 using CryptoQuest.Sagas.MagicStone;
 using IndiGames.Core.Common;
 using IndiGames.Core.Events;
+using UnityEngine;
 
 namespace CryptoQuest.BlackSmith.UpgradeStone.Sagas
 {
-    public class GetStonesResponsedHandler : SagaBase<FetchIngameMagicStonesSuccess>
+    public class GetStonesResponsedHandler : SagaBase<GetStonesResponsed>
     {
         private IMagicStoneResponseConverter _converter;
 
-        protected override void HandleAction(FetchIngameMagicStonesSuccess ctx)
+        protected override void HandleAction(GetStonesResponsed ctx)
         {
             var stones = new List<IMagicStone>();
 
             _converter ??= ServiceProvider.GetService<IMagicStoneResponseConverter>();
-            foreach (var data in ctx.Stones)
+            foreach (var data in ctx.Response.data.stones)
             {
                 if (data.inGameStatus != 2) continue;
                 var magicStone = _converter.Convert(data);
