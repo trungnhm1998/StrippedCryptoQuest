4b11f11393dd88fdfe50b915d19333f01c3f1360
Long18 <us.thanhlong18@gmail.com>
2023-12-22 13:01:47 +0700

    fix(quest-system): actor condition

--- Assets/Scripts/Quest/ActorSettingSO.cs
+++ Assets/Scripts/Quest/ActorSettingSO.cs
@@ -16,15 +16,14 @@ namespace CryptoQuest.Quest
     [CreateAssetMenu(fileName = "ActorSettingSO", menuName = "QuestSystem/Quests/ActorSetting/Actor Setting")]
     public class ActorSettingSO : ScriptableObject, IQuestConfigure
     {
-        public Action<bool> OnConfigure;
-        public Action OnQuestCompleted;
-
         [field: SerializeField] public EConditionType QuestCondition { get; set; } = EConditionType.Required;
 
         [field: SerializeField]
         public List<QuestSO> QuestsToTrack { get; set; } = new();
 
-        public virtual void Configure(bool isQuestCompleted, int questCompletedCount) { }
+        public virtual void Configure(bool isQuestCompleted, string questsCompleted) { }
+        public virtual void CompleteQuest(string questGuid) { }
+
         public ActorSettingInfo CreateActorSettingInfo() => new(this);
     }
 
@@ -32,22 +31,37 @@ namespace CryptoQuest.Quest
     {
         public Action<bool> OnConfigure;
         public Action OnQuestCompleted;
-        private int _questCompletedCount;
         public EConditionType QuestCondition { get; set; }
         public List<QuestSO> QuestsToTrack { get; set; }
+
+        private readonly Dictionary<string, bool> _questCompletedDict;
 
         public ActorSettingInfo(ActorSettingSO data)
         {
             QuestCondition = data.QuestCondition;
             QuestsToTrack = data.QuestsToTrack;
+            _questCompletedDict = QuestsToTrack.ToDictionary(quest => quest.Guid, quest => false);
         }
 
-        public void Configure(bool isQuestCompleted, int questCompletedCount)
+        public void Configure(bool isQuestCompleted, string questsCompleted)
         {
-            _questCompletedCount = questCompletedCount;
-            OnConfigure?.Invoke(isQuestCompleted);
+            OnConfigureQuestHandler();
 
-            if (!isQuestCompleted) return;
+            if (string.IsNullOrEmpty(questsCompleted) || !_questCompletedDict.ContainsKey(questsCompleted)) return;
+
+            _questCompletedDict[questsCompleted] = isQuestCompleted;
+
+            if ((QuestCondition != EConditionType.And || !_questCompletedDict.All(pair => pair.Value)) &&
+                (QuestCondition == EConditionType.And || !_questCompletedDict.Any(pair => pair.Value))) return;
+
+            OnQuestCompletedHandler();
+        }
+
+        public void CompleteQuest(string questGuid)
+        {
+            if (!_questCompletedDict.ContainsKey(questGuid)) return;
+
+            _questCompletedDict[questGuid] = true;
             OnQuestCompletedHandler();
         }
 
@@ -57,21 +71,18 @@ namespace CryptoQuest.Quest
         public void Unsubscribe() =>
             QuestsToTrack.ForEach(questData => questData.OnQuestCompleted -= OnQuestCompletedHandler);
 
+        private void OnConfigureQuestHandler()
+        {
+            bool conditionMet = QuestCondition == EConditionType.And
+                ? _questCompletedDict.All(pair => pair.Value)
+                : _questCompletedDict.Any(pair => pair.Value);
+
+            OnConfigure?.Invoke(conditionMet);
+        }
+
         private void OnQuestCompletedHandler()
         {
-            _questCompletedCount++;
-            switch (QuestCondition)
-            {
-                default:
-                case EConditionType.Required:
-                case EConditionType.Or:
-                    OnQuestCompleted?.Invoke();
-                    break;
-                case EConditionType.And:
-                    if (_questCompletedCount != QuestsToTrack.Count) return;
-                    OnQuestCompleted?.Invoke();
-                    break;
-            }
+            OnQuestCompleted?.Invoke();
         }
     }
 }
