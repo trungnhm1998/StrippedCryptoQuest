65091687b17f57df6d3a9a82c44fa5c9745c6819
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-12-22 15:55:04 +0700

    feat: remove login email, add skip login button

---
+++ Assets/Scripts/Sagas/EditorAuthenticate.cs
@@ -0,0 +1,20 @@
+﻿using CryptoQuest.Actions;
+using IndiGames.Core.Events;
+using UnityEngine;
+
+namespace CryptoQuest.Sagas
+{
+    public class EditorAuthenticate : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Debug Account Login"))
+                ActionDispatcher.Dispatch(new DebugLoginAction());
+
+            if (GUILayout.Button("Skip Login"))
+                ActionDispatcher.Dispatch(new AuthenticateSucceed());
+        }
+#endif
+    }
+}
