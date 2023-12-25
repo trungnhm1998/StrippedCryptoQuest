755dbf7dec1df9446ebc2c6b02293e832a3282c4
minhnguyen0699 <minh.nguyen@indigames.net>
2023-12-25 15:05:38 +0700

    feat: update quest kpi

---
+++ Assets/Scripts/Sagas/Objects/EventLogResponse.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CryptoQuest.Sagas.Objects
+{
+    [Serializable]
+    public class EventLogResponse
+    {
+        public int code { get; set; }
+        public bool success { get; set; }
+        public string message { get; set; }
+        public string uuid { get; set; }
+        public int gold { get; set; }
+        public int diamond { get; set; }
+        public int soul { get; set; }
+        public long time { get; set; }
+    }
+}
