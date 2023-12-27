ddd7883c05cc963973616ab3a4b3c0055d52c8fb
lananhcao <anh.cao@indigames.net>
2023-12-27 17:19:30 +0700

    fix: update id of non-nft hero following server

---
+++ Assets/Scripts/Sagas/Party/SaveParty.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CryptoQuest.API;
+using CryptoQuest.Networking;
+using CryptoQuest.Sagas.Objects;
+using CryptoQuest.UI.Actions;
+using IndiGames.Core.Common;
+using IndiGames.Core.Events;
+using UniRx;
+using UnityEngine;
+using NotImplementedException = System.NotImplementedException;
+
+namespace CryptoQuest.Sagas.Party
+{
+    public class SaveParty : SagaBase<SavePartyAction>
+    {
+        protected override void HandleAction(SavePartyAction ctx)
+        {
+            ActionDispatcher.Dispatch(new ShowLoading());
+            var restClient = ServiceProvider.GetService<IRestClient>();
+            restClient
+                .Get<CharactersResponse>(CharacterAPI.GET_CHARACTERS)
+                .Subscribe(ProcessResponseCharacters, OnError);
+        }
+
+        private void ProcessResponseCharacters(CharactersResponse obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnError(Exception obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
