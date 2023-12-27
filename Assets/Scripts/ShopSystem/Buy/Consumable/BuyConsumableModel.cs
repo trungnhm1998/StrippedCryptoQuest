e8a21fedc718af74b7e6947d9064ca13e903249c
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2023-12-27 15:35:50 +0700

    feat(shop): buy consumable ui

---
+++ Assets/Scripts/ShopSystem/Buy/Consumable/BuyConsumableModel.cs
@@ -0,0 +1,11 @@
+﻿using CryptoQuest.Item.Consumable;
+using UnityEngine;
+
+namespace CryptoQuest.ShopSystem.Buy.Consumable
+{
+    public class BuyConsumableModel : MonoBehaviour
+    {
+        [SerializeField] private ConsumableSO[] _sellingConsumables;
+        public ConsumableSO[] SellingConsumables => _sellingConsumables;
+    }
+}
---
+++ Assets/Scripts/ShopSystem/Buy/Consumable/BuyConsumablePresenter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace CryptoQuest.ShopSystem.Buy.Consumable
+{
+    public class BuyConsumablePresenter : MonoBehaviour
+    {
+        [SerializeField] private BuyConsumableModel _model;
+        [SerializeField] private UIBuyConsumableList _uiBuyableList;
+
+        private void OnEnable()
+        {
+            _uiBuyableList.BuyingConsumable += BuyConsumable;
+            _uiBuyableList.Render(_model.SellingConsumables);
+        }
+
+        private void OnDisable()
+        {
+            _uiBuyableList.BuyingConsumable -= BuyConsumable;
+        }
+
+        private void BuyConsumable(UIConsumableShopItem item) { }
+    }
+}
