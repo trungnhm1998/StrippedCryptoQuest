0b54881efe6f005263d169893dbd1e3401739c8f
Viet <viet.hoang@indigames.net>
2024-01-09 12:06:20 +0900

    feat(shop): consumable details

---
+++ Assets/Scripts/ShopSystem/ConsumableDetailsPresenter.cs
@@ -0,0 +1,37 @@
+﻿using IndiGames.Core.Events.ScriptableObjects;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace CryptoQuest.ShopSystem
+{
+    public class ConsumableDetailsPresenter : MonoBehaviour
+    {
+        [SerializeField] private UIConsumableDetails _uiConsumableDetails;
+        [SerializeField] private BoolEventChannelSO _showConsumableDetailsEvent;
+
+        private void OnEnable()
+        {
+            _showConsumableDetailsEvent.EventRaised += SetActiveConsumableDetails;
+        }
+
+        private void OnDisable()
+        {
+            _showConsumableDetailsEvent.EventRaised -= SetActiveConsumableDetails;
+        }
+        
+        private void SetActiveConsumableDetails(bool show)
+        {
+            _uiConsumableDetails.gameObject.SetActive(show);
+            if (show)
+                SetDetailUI();
+        }
+
+        private void SetDetailUI()
+        {
+            var selectedObject = EventSystem.current.currentSelectedGameObject;
+            if (selectedObject == null) return;
+            var provider = selectedObject.GetComponent<IConsumableInfoProvider>();
+            _uiConsumableDetails.SetupUI(provider.ConsumableInfo);
+        }
+    }
+}
---
+++ Assets/Scripts/ShopSystem/UIConsumableDetails.cs
@@ -0,0 +1,22 @@
+﻿using CryptoQuest.Item.Consumable;
+using CryptoQuest.UI.Extensions;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+using UnityEngine.UI;
+
+namespace CryptoQuest.ShopSystem
+{
+    public class UIConsumableDetails : MonoBehaviour
+    {
+        [SerializeField] private Image _icon;
+        [SerializeField] private LocalizeStringEvent _name;
+        [SerializeField] private LocalizeStringEvent _description;
+
+        public void SetupUI(ConsumableInfo consumable)
+        {
+            _icon.LoadSpriteAndSet(consumable.Icon);
+            _name.StringReference = consumable.DisplayName;
+            _description.StringReference = consumable.Description;
+        }
+    }
+}
