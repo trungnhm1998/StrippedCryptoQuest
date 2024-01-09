8ee4e234a7485dba8639b3aebb70d0c81a051299
Viet <viet.hoang@indigames.net>
2024-01-09 12:06:20 +0900

    feat(shop): consumable details

---
+++ Assets/Scripts/ShopSystem/ConsumableItemProvider.cs
@@ -0,0 +1,35 @@
+﻿using CryptoQuest.Item.Consumable;
+using IndiGames.Core.Events.ScriptableObjects;
+using UnityEngine;
+
+namespace CryptoQuest.ShopSystem
+{
+    public interface IConsumableInfoProvider
+    {
+        ConsumableInfo ConsumableInfo { get; }
+    }
+
+    public class ConsumableItemProvider : MonoBehaviour, IConsumableInfoProvider
+    {
+        [SerializeField] private UIConsumableShopItem _uiConsumableShopItem;
+        [SerializeField] private BoolEventChannelSO _showConsumableDetailsEvent;
+
+        public ConsumableInfo ConsumableInfo => _uiConsumableShopItem.Info;
+
+        private void OnValidate()
+        {
+            if (_uiConsumableShopItem == null)
+                _uiConsumableShopItem = GetComponent<UIConsumableShopItem>();
+        }
+        
+        public void OnSelected()
+        {
+            _showConsumableDetailsEvent.RaiseEvent(true);
+        }
+        
+        private void OnDisable()
+        {
+            _showConsumableDetailsEvent.RaiseEvent(false);
+        }
+    }
+}
