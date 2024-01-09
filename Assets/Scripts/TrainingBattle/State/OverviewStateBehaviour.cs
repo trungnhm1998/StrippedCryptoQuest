dafc8d5f2c966cd51d94ea15ef516fcce243c781
datdiep754129 <104046069+datdiep754129@users.noreply.github.com>
2024-01-09 15:02:57 +0700

    feat: implement training battle npc (#3384)

---
+++ Assets/Scripts/TrainingBattle/State/OverviewStateBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace CryptoQuest.TrainingBattle.State
+{
+    public class OverviewStateBehaviour : BaseStateBehaviour
+    {
+        private TrainingBattleStateController _stateController;
+        private static readonly int AcceptBattle = Animator.StringToHash("AcceptBattleState");
+        private static readonly int CancelBattle = Animator.StringToHash("CancelBattleState");
+
+        protected override void OnEnter()
+        {
+            _stateController = StateMachine.GetComponent<TrainingBattleStateController>();
+            _stateController.DialogController.YesPressedEvent += ChangeState;
+            _stateController.DialogController.NoPressedEvent += ExitState;
+            _stateController.Input.CancelEvent += ExitState;
+            _stateController.IsExitState = false;
+        }
+
+        protected override void OnExit()
+        {
+            _stateController.DialogController.YesPressedEvent -= ChangeState;
+            _stateController.DialogController.NoPressedEvent -= ExitState;
+            _stateController.Input.CancelEvent -= ExitState;
+        }
+
+        private void ChangeState()
+        {
+            StateMachine.Play(AcceptBattle);
+            _stateController.DialogController.ChoiceDialog.Hide();
+        }
+
+        private void ExitState()
+        {
+            StateMachine.Play(CancelBattle);
+            _stateController.IsExitState = true;
+            _stateController.DialogController.ChoiceDialog.Hide();
+        }
+    }
+}
