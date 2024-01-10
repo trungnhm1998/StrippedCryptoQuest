399a00e0df1d48b67b6e97d041319de143e929cf
datdiep754129 <dat.diep@indigames.net>
2024-01-10 14:15:08 +0700

    fix: fetch equipment to inventory after transferred

---
+++ Assets/Scripts/Menus/DimensionalBox/Sagas/UpdateInventoryOnTransferSuccessSaga.cs
@@ -0,0 +1,38 @@
+﻿using CryptoQuest.Inventory.Actions;
+using CryptoQuest.Item.Equipment;
+using CryptoQuest.Sagas.Equipment;
+using CryptoQuest.Sagas.Objects;
+using IndiGames.Core.Common;
+using IndiGames.Core.Events;
+
+namespace CryptoQuest.Menus.DimensionalBox.Sagas
+{
+    public class UpdateEquipmentAction : ActionBase
+    {
+        public EquipmentResponse[] Equipments { get; }
+
+        public UpdateEquipmentAction(EquipmentResponse[] equipments)
+        {
+            Equipments = equipments;
+        }
+    }
+
+    public class UpdateInventoryOnTransferSuccessSaga : SagaBase<UpdateEquipmentAction>
+    {
+        protected override void HandleAction(UpdateEquipmentAction ctx)
+        {
+            var converter = ServiceProvider.GetService<IEquipmentResponseConverter>();
+            foreach (var equipmentResponse in ctx.Equipments)
+            {
+                var equipment = converter.Convert(equipmentResponse);
+                if (equipmentResponse.inGameStatus == (int)EEquipmentStatus.InGame)
+                    AddEquipment(equipment);
+                else
+                    RemoveEquipment(equipment);
+            }
+        }
+
+        private void AddEquipment(IEquipment equipment) => ActionDispatcher.Dispatch(new AddEquipmentAction(equipment));
+        private void RemoveEquipment(IEquipment equipment) => ActionDispatcher.Dispatch(new RemoveEquipmentAction(equipment));
+    }
+}
