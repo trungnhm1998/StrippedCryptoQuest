017689a84027465c1cf9303133b49309845e9300
Long18 <us.thanhlong18@gmail.com>
2024-01-18 15:40:57 +0700

    fix(build-tool): get config from UserBuildSettings

--- Assets/CryptoQuestEditor/Scripts/EditorTool/BuildAndRunEditorWindow.cs
+++ Assets/CryptoQuestEditor/Scripts/EditorTool/BuildAndRunEditorWindow.cs
@@ -1,22 +1,22 @@
 using System.Diagnostics;
 using System.IO;
-using CryptoQuestEditor.EditorTool;
-using IndiGamesEditor.UnityBuilderAction.Versioning;
 using UnityEditor;
 using UnityEditor.AddressableAssets.Build;
 using UnityEditor.AddressableAssets.Settings;
 using UnityEditor.Build.Reporting;
-using UnityEngine;
 using Debug = UnityEngine.Debug;
 
 namespace CryptoQuestEditor.CryptoQuestEditor.Scripts.EditorTool
 {
     public class BuildAndRunEditorWindow : EditorWindow
     {
-        const string application = @"cmd.exe";
-
         private const string FIRST_SCENE_PATH = "Assets/Scenes/Startup.unity";
         private const string SERVER_LOCAL_NAME = "ServerLocal.bat";
+
+        private const string DISABLE_ECHO = "echo off";
+        private const string CHANGE_DIRECTORY = "cd";
+        private const string START_LOCALHOST = "start /max \"\"\"\" http://localhost";
+        private const string START_PYTHON_HTTP_SERVER = "python -m http.server 80";
 
         [MenuItem("Crypto Quest/Build and Run %#b")]
         public static void BuildAndRun()
@@ -30,13 +30,20 @@ namespace CryptoQuestEditor.CryptoQuestEditor.Scripts.EditorTool
             string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
             if (path.Length == 0) return;
 
-            if (!BuildAddressable()) return;
+            // Prevent domain reload during critical sections
+            EditorApplication.LockReloadAssemblies();
 
-            if (!Build(path)) return;
-
-            Debug.Log($"<color=green>[Build]</color> Build success: {path}");
-
-            CreateBatFileAndRun(path);
+            try
+            {
+                if (!BuildAddressable()) return;
+                if (!Build(path)) return;
+                CreateBatFileAndRun(path);
+            }
+            finally
+            {
+                // Allow domain reload after critical sections
+                EditorApplication.UnlockReloadAssemblies();
+            }
         }
 
         private void CreateBatFileAndRun(string path)
@@ -47,7 +54,8 @@ namespace CryptoQuestEditor.CryptoQuestEditor.Scripts.EditorTool
 
             if (!File.Exists(fullPath))
             {
-                Debug.LogError($"{SERVER_LOCAL_NAME} file not found at the specified location.");
+                Debug.LogWarning(
+                    $"<color=red>[CreateBatFileAndRun]</color> {SERVER_LOCAL_NAME} file not found at {fullPath}i.");
                 return;
             }
 
@@ -58,10 +66,10 @@ namespace CryptoQuestEditor.CryptoQuestEditor.Scripts.EditorTool
         {
             StreamWriter writer = new StreamWriter($"{path}/{SERVER_LOCAL_NAME}", false);
 
-            writer.WriteLine("echo off");
-            writer.WriteLine($"cd {path}");
-            writer.WriteLine("start /max \"\"\"\" http://localhost");
-            writer.WriteLine("python -m http.server 80");
+            writer.WriteLine(DISABLE_ECHO);
+            writer.WriteLine($"{CHANGE_DIRECTORY} {path}");
+            writer.WriteLine(START_LOCALHOST);
+            writer.WriteLine(START_PYTHON_HTTP_SERVER);
             writer.Close();
         }
 
@@ -71,45 +79,37 @@ namespace CryptoQuestEditor.CryptoQuestEditor.Scripts.EditorTool
             {
                 scenes = new[] { FIRST_SCENE_PATH },
                 locationPathName = path,
-                target = BuildTarget.WebGL,
-                options = BuildOptions.Development | BuildOptions.AllowDebugging,
-                targetGroup = BuildTargetGroup.WebGL
+                target = EditorUserBuildSettings.activeBuildTarget,
+                options = EditorUserBuildSettings.development
+                    ? BuildOptions.Development | BuildOptions.AllowDebugging
+                    : BuildOptions.None,
+                targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup,
             };
-
-            BuildPipeline.BuildPlayer(buildPlayerOptions);
 
             BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
             BuildSummary summary = report.summary;
 
-            switch (summary.result)
+            if (summary.result == BuildResult.Succeeded) return true;
+            Debug.LogWarning($"<color=red>[Build]</color> Build failed: {summary.result}");
+
+            foreach (var step in report.steps)
             {
-                case BuildResult.Failed:
-                    Debug.Log($"<color=red>[Build]</color> Build error: {summary.totalErrors}");
-                    return false;
-                case BuildResult.Cancelled:
-                    Debug.Log($"<color=yellow>[Build]</color> Build stop: {summary.totalErrors}");
-                    return false;
-                case BuildResult.Unknown:
-                    Debug.Log($"<color=purple>[Build]</color> Build unknown: {summary.totalErrors}");
-                    return false;
-                case BuildResult.Succeeded:
-                default:
-                    return true;
+                if (summary.result != BuildResult.Failed) continue;
+                Debug.LogWarning($"<color=red>[Build]</color> Step '{step.name}' failed: {step.messages}");
             }
+
+            return false;
         }
 
         private bool BuildAddressable()
         {
-            AddressableAssetSettings
-                .BuildPlayerContent(out AddressablesPlayerBuildResult result);
+            AddressableAssetSettings.BuildPlayerContent(out AddressablesPlayerBuildResult result);
             bool success = string.IsNullOrEmpty(result.Error);
 
-            if (!success)
-            {
-                Debug.Log($"<color=red>[Addressables]</color> Build error: {result.Error}");
-            }
+            if (success) return true;
+            Debug.Log($"<color=red>[BuildAddressable]</color> Build error: {result.Error}");
 
-            return success;
+            return false;
         }
     }
 }
