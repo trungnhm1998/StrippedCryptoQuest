fe45b484efe25d9f2c593469404257c309d377c2
Viet <viet.hoang@indigames.net>
2024-01-24 18:09:18 +0900

    fix(battle): logger when cast skill fail (QQM_DEV-457)

---
+++ Assets/Scripts/Battle/UI/Logs/CastSkillFailedLogger.cs
@@ -0,0 +1,28 @@
+﻿using CryptoQuest.Battle.Events;
+using TinyMessenger;
+using UnityEngine;
+using UnityEngine.Localization;
+
+namespace CryptoQuest.Battle.UI.Logs
+{
+    public class CastSkillFailedLogger : LoggerComponentBase
+    {
+        [SerializeField] private LocalizedString _failedMessage;
+        private TinyMessageSubscriptionToken _failedEvent;
+
+        private void Awake()
+        {
+            _failedEvent = BattleEventBus.SubscribeEvent<CastSkillFailedEvent>(LogCastFailed);
+        }
+
+        private void OnDestroy()
+        {
+            BattleEventBus.UnsubscribeEvent(_failedEvent);
+        }
+        
+        private void LogCastFailed(CastSkillFailedEvent ctx)
+        {
+            Logger.QueueLog(_failedMessage);
+        }
+    }
+}
