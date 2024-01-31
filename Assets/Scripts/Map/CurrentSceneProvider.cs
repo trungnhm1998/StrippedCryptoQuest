782e5fe899c40082ef599040a72010216f179af2
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2024-01-31 14:53:53 +0700

    style(tavern): select indicator

--- Assets/Scripts/Map/CurrentSceneProvider.cs
+++ Assets/Scripts/Map/CurrentSceneProvider.cs
@@ -1,26 +1,25 @@
-using System.Collections;
-using CryptoQuest.System;
-using IndiGames.Core.Common;
 using IndiGames.Core.SceneManagementSystem.Events.ScriptableObjects;
 using IndiGames.Core.SceneManagementSystem.ScriptableObjects;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 
 namespace CryptoQuest.Map
 {
     public class CurrentSceneProvider : MonoBehaviour
     {
+        [SerializeField] private LoadSceneEventChannelSO _coldBootEvent;
         [SerializeField] private LoadSceneEventChannelSO _loadSceneEvent;
         [SerializeField] private SceneManagerSO _sceneManagerSO;
 
         private void OnEnable()
         {
             _loadSceneEvent.LoadingRequested += OnLoadScene;
+            _coldBootEvent.LoadingRequested += OnLoadScene;
         }
 
         private void OnDisable()
         {
             _loadSceneEvent.LoadingRequested -= OnLoadScene;
+            _coldBootEvent.LoadingRequested -= OnLoadScene;
         }
 
         private void OnLoadScene(SceneScriptableObject scene)
